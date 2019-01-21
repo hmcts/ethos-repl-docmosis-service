b6a14f68ccd6209f7d1dd91234616680e638fdf8
Javier Carretero <javi_1986@hotmail.com>
2019-01-21 10:21:34 +0000

    First commit

---
+++ src/main/java/uk/gov/hmcts/ethos/replacement/docmosis/DocmosisApplication.java
@@ -0,0 +1,14 @@
+package uk.gov.hmcts.ethos.replacement.docmosis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocmosisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DocmosisApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/uk/gov/hmcts/ethos/replacement/docmosis/DocmosisApplicationTests.java
@@ -0,0 +1,17 @@
+package uk.gov.hmcts.ethos.replacement.docmosis;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DocmosisApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
