ac180ac71d06cc7fe31c7478c76ab788930edb06
Javier Carretero <javi_1986@hotmail.com>
2019-02-21 09:20:30 +0000

    Fixed the collections of elements

---
+++ src/main/java/uk/gov/hmcts/ethos/replacement/docmosis/model/ccd/items/HearingTypeItem.java
@@ -0,0 +1,17 @@
+package uk.gov.hmcts.ethos.replacement.docmosis.model.ccd.items;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+import uk.gov.hmcts.ethos.replacement.docmosis.model.ccd.types.HearingType;
+import uk.gov.hmcts.ethos.replacement.docmosis.model.ccd.types.PanelType;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+@Data
+public class HearingTypeItem {
+
+    @JsonProperty("id")
+    private String id;
+    @JsonProperty("value")
+    private HearingType value;
+}
