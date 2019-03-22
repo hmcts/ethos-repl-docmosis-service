f91339e6fc081db26dafe7d72026017290700ad4
Javier Carretero <javi_1986@hotmail.com>
2019-03-22 13:46:23 +0000

    Updated vaultGroupName

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -10,9 +10,9 @@ locals {
   nonPreviewVaultName = "${var.product}-${var.env}"
   vaultName = "${var.env == "preview" ? local.previewVaultName : local.nonPreviewVaultName}"
 //  vaultUri = "${data.azurerm_key_vault.ethos_key_vault.vault_uri}"
-//  previewVaultGroupName = "${var.product}-${local.app}-aat"
-//  nonPreviewVaultGroupName = "${var.product}-${local.app}-${var.env}"
-//  vaultGroupName = "${var.env == "preview" ? local.previewVaultGroupName : local.nonPreviewVaultGroupName}"
+  previewVaultGroupName = "${var.product}-${local.app}-aat"
+  nonPreviewVaultGroupName = "${var.product}-${local.app}-${var.env}"
+  vaultGroupName = "${var.env == "preview" ? local.previewVaultGroupName : local.nonPreviewVaultGroupName}"
 
   # list of the thumbprints of the SSL certificates that should be accepted by the API (gateway)
 //  allowed_certificate_thumbprints = [
@@ -63,7 +63,7 @@ module "key-vault" {
   tenant_id               = "${var.tenant_id}"
   object_id               = "${var.jenkins_AAD_objectId}"
   //resource_group_name     = "${module.repl-docmosis-backend.resource_group_name}"
-  resource_group_name     = "${local.vaultName}"
+  resource_group_name     = "${local.vaultGroupName}"
   # dcd_cc-dev group object ID
   product_group_object_id = "38f9dea6-e861-4a50-9e73-21e64f563537"
   common_tags             = "${var.common_tags}"
