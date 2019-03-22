450c7caf4cccf967388979488e2b9ae0532bd018
Javier Carretero <javi_1986@hotmail.com>
2019-03-22 09:29:35 +0000

    Uncommented certificate

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -7,15 +7,15 @@ locals {
   create_api = "${var.env != "preview" && var.env != "spreview"}"
 
   # list of the thumbprints of the SSL certificates that should be accepted by the API (gateway)
-//  allowed_certificate_thumbprints = [
-//    # API tests
-//    "${var.api_gateway_test_certificate_thumbprint}"
-//  ]
-//
-//  thumbprints_in_quotes = "${formatlist("&quot;%s&quot;", local.allowed_certificate_thumbprints)}"
-//  thumbprints_in_quotes_str = "${join(",", local.thumbprints_in_quotes)}"
-//  api_policy = "${replace(file("template/api-policy.xml"), "ALLOWED_CERTIFICATE_THUMBPRINTS", local.thumbprints_in_quotes_str)}"
-//  api_base_path = "ethos-repl-docmosis-service"
+  allowed_certificate_thumbprints = [
+    # API tests
+    "${var.api_gateway_test_certificate_thumbprint}"
+  ]
+
+  thumbprints_in_quotes = "${formatlist("&quot;%s&quot;", local.allowed_certificate_thumbprints)}"
+  thumbprints_in_quotes_str = "${join(",", local.thumbprints_in_quotes)}"
+  api_policy = "${replace(file("template/api-policy.xml"), "ALLOWED_CERTIFICATE_THUMBPRINTS", local.thumbprints_in_quotes_str)}"
+  api_base_path = "ethos-repl-docmosis-service"
 }
 
 module "repl-docmosis-backend" {
@@ -55,23 +55,23 @@ module "key-vault" {
 
 # region API (gateway)
 
-//data "template_file" "api_template" {
-//  template = "${file("${path.module}/template/api.json")}"
-//}
-//
-//resource "azurerm_template_deployment" "api" {
-//  template_body       = "${data.template_file.api_template.rendered}"
-//  name                = "${var.product}-api-${var.env}"
-//  deployment_mode     = "Incremental"
-//  resource_group_name = "core-infra-${var.env}"
-//  count               = "${local.create_api ? 1 : 0}"
-//
-//  parameters = {
-//    apiManagementServiceName  = "core-api-mgmt-${var.env}"
-//    apiName                   = "ethos-repl-docmosis-service"
-//    apiProductName            = "ethos-repl-docmosis"
-//    serviceUrl                = "http://${var.product}-${local.app}-${var.env}.service.core-compute-${var.env}.internal"
-////    apiBasePath               = "${local.api_base_path}"
-////    policy                    = "${local.api_policy}"
-//  }
-//}
+data "template_file" "api_template" {
+  template = "${file("${path.module}/template/api.json")}"
+}
+
+resource "azurerm_template_deployment" "api" {
+  template_body       = "${data.template_file.api_template.rendered}"
+  name                = "${var.product}-api-${var.env}"
+  deployment_mode     = "Incremental"
+  resource_group_name = "core-infra-${var.env}"
+  count               = "${local.create_api ? 1 : 0}"
+
+  parameters = {
+    apiManagementServiceName  = "core-api-mgmt-${var.env}"
+    apiName                   = "ethos-repl-docmosis-service"
+    apiProductName            = "ethos-repl-docmosis"
+    serviceUrl                = "http://${var.product}-${local.app}-${var.env}.service.core-compute-${var.env}.internal"
+    apiBasePath               = "${local.api_base_path}"
+    policy                    = "${local.api_policy}"
+  }
+}
