ea4204da680b9763cee49a4f979aae17f35b1bf6
Javier Carretero <javi_1986@hotmail.com>
2019-03-29 09:07:45 +0000

    Added secrets

--- src/main/java/uk/gov/hmcts/ethos/replacement/docmosis/service/DocumentGenerationService.java
+++ src/main/java/uk/gov/hmcts/ethos/replacement/docmosis/service/DocumentGenerationService.java
@@ -2,6 +2,7 @@ package uk.gov.hmcts.ethos.replacement.docmosis.service;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import uk.gov.hmcts.ethos.replacement.docmosis.exceptions.DocumentManagementException;
 import uk.gov.hmcts.ethos.replacement.docmosis.model.ccd.CCDRequest;
@@ -15,6 +16,9 @@ public class DocumentGenerationService {
     private final TornadoService tornadoService;
     private static final String MESSAGE = "Failed to generate document for case id : ";
 
+    @Value("${azure.app_insights_key}")
+    private String appInsightsKey;
+
     @Autowired
     public DocumentGenerationService(TornadoService tornadoService) {
         this.tornadoService = tornadoService;
@@ -24,6 +28,8 @@ public class DocumentGenerationService {
         CaseDetails caseDetails = ccdRequest.getCaseDetails();
         log.info("Auth Token: " + authToken);
         log.info("Case Details: " + caseDetails);
+        log.info("AppInsightsKey: " + "162CD82F0363F36E81218AD218C3C19F862CD479");
+        log.info("AppInsightsKey: " + appInsightsKey);
         try {
             return tornadoService.documentGeneration(authToken, ccdRequest.getCaseDetails());
         } catch (Exception ex) {
