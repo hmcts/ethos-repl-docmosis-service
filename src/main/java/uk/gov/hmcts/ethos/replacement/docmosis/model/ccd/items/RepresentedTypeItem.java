e57abd21b14f9b0f55b278c1006806aee60d253f
Javier Carretero <javi_1986@hotmail.com>
2019-04-08 14:39:10 +0100

    Updated with latest spreadsheet

--- src/main/java/uk/gov/hmcts/ethos/replacement/docmosis/model/ccd/items/RepresentedTypeItem.java
+++ src/main/java/uk/gov/hmcts/ethos/replacement/docmosis/model/ccd/items/RepresentedTypeItem.java
@@ -3,14 +3,14 @@ package uk.gov.hmcts.ethos.replacement.docmosis.model.ccd.items;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
-import uk.gov.hmcts.ethos.replacement.docmosis.model.ccd.types.RespondentSumType;
+import uk.gov.hmcts.ethos.replacement.docmosis.model.ccd.types.RepresentedType;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 @Data
-public class RespondentSumTypeItem {
+public class RepresentedTypeItem {
 
     @JsonProperty("id")
     private String id;
     @JsonProperty("value")
-    private RespondentSumType value;
+    private RepresentedType value;
 }
