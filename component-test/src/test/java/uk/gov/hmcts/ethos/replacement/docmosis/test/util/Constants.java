7fca3587eb232e8caeb351d6a9b55c6d72132d5e
kiran <kiran.pannala@hmcts.net>
2019-05-10 10:34:50 +0100

    Created the component-test module and wrote a test case

---
+++ component-test/src/test/java/uk/gov/hmcts/ethos/replacement/docmosis/test/util/Constants.java
@@ -0,0 +1,7 @@
+package uk.gov.hmcts.ethos.replacement.docmosis.test.util;
+
+public class Constants {
+    public static final String BASE_URL = "http://localhost:8081";
+    public static final String URL_GEN_DOCUMENT = "/generateDocument";
+    public static final String TEST_DATA_CASE1 = "src/test/resources/testData/caseDetailsTest1.json";
+}
