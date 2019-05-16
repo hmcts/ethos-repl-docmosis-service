934ae99e6bab34956e876cbf9107d685cb1b1697
kiran <kiran.pannala@hmcts.net>
2019-05-16 15:42:14 +0100

    Wrote code to read expected template and check if each field contains expected value

---
+++ component-test/src/test/java/uk/gov/hmcts/ethos/replacement/docmosis/test/util/JsonUtil.java
@@ -0,0 +1,32 @@
+package uk.gov.hmcts.ethos.replacement.docmosis.test.util;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import uk.gov.hmcts.ethos.replacement.docmosis.model.ccd.CCDRequest;
+import uk.gov.hmcts.ethos.replacement.docmosis.model.ccd.CaseDetails;
+
+import java.io.IOException;
+
+public class JsonUtil {
+
+    public static String getValue(String testDataJson, String key) throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode rootNode = mapper.readTree(testDataJson);
+
+        JsonNode jsonNode = rootNode.findValue(key);
+
+        if (jsonNode != null) return jsonNode.textValue();
+
+        return null;
+    }
+
+    public static CCDRequest getCaseDetails(String json, String topLevel, String childLevel) throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+
+        json = json.replace("#TOPLEVEL#", "Part_" + topLevel);
+        String templateVersion = topLevel + "." + childLevel;
+        json = json.replace("#VERSION#", templateVersion);
+
+        return mapper.readValue(json, CCDRequest.class);
+    }
+}
