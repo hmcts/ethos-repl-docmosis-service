ff41ab82e9a3ba4ddd1ea0c835be5acb0fd94ee0
kiran <kiran.pannala@hmcts.net>
2019-05-29 15:05:50 +0100

    Updated docker-setup.sh script to use master branch and added sql to add roles for new jurisdiction

--- component-test/docker-setup.sh
+++ component-test/docker-setup.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
 
-##############################################################################################
-# It is assumed that "az login" command has already been executed before running this script #
-##############################################################################################
+#############################################################################################
+# Assumptions:										                                        #
+# 1. VPN connected									                                        #
+# 2. "az login" command executed							                                #
+#############################################################################################
+
+#Install postgres client
+sudo apt-get install postgresql-client -y
 
 #Clone ccd-docker repo if necessary
 if [[ ! -d "../../ccd-docker" ]]; then
@@ -15,11 +20,11 @@ fi
 ../../ccd-docker/ccd login
 
 #Create docker images for projects below using "demo" branch
-../../ccd-docker/ccd set ccd-data-store-api demo
-../../ccd-docker/ccd set ccd-definition-store-api demo
-../../ccd-docker/ccd set ccd-user-profile-api demo
-../../ccd-docker/ccd set ccd-api-gateway demo
-../../ccd-docker/ccd set ccd-case-management-web demo
+#../../ccd-docker/ccd set ccd-data-store-api demo
+#../../ccd-docker/ccd set ccd-definition-store-api demo
+#../../ccd-docker/ccd set ccd-user-profile-api demo
+#../../ccd-docker/ccd set ccd-api-gateway demo
+#../../ccd-docker/ccd set ccd-case-management-web demo
 
 #Pull necessary images from docker registry
 ../../ccd-docker/ccd compose pull
@@ -40,10 +45,14 @@ done
 echo "All Services started successfully"
 echo ""
 
+#Add roles to Idam
+psql -h localhost -p 5000 -U ccd -d idam -c "insert into role values ('caseworker-employment-tribunal-manchester','Caseworker Employment Tribunal Manchester'),('caseworker-employment','Caseworker Employment'),('caseworker-employment-tribunal-manchester-caseofficer','Caseworker Employment Tribunal Manchester Caseofficer')"
+
+
 #Execute these steps manually
-#../../ccd-docker/bin/idam-create-caseworker.sh caseworker,caseworker-test,caseworker-employment-tribunal-manchester,caseworker-employment,caseworker-employment-tribunal-manchester-caseofficer eric.la.cooper@gmail.com password Cooper Eric
-#../../ccd-docker/bin/ccd-add-role.sh caseworker-employment-tribunal-manchester
-#../../ccd-docker/bin/ccd-add-role.sh caseworker-employment
-#../../ccd-docker/bin/ccd-add-role.sh caseworker-employment-tribunal-manchester-caseofficer
-#../../ccd-docker/bin/ccd-add-role.sh caseworker-employment-tribunal-manchester-casesupervisor
-#../../ccd-docker/bin/ccd-import-definition.sh src/test/resources/config/CCD_EmpTrib_Manc_v1.0.09.xlsx
+../../ccd-docker/bin/idam-create-caseworker.sh caseworker,caseworker-test,caseworker-employment-tribunal-manchester,caseworker-employment,caseworker-employment-tribunal-manchester-caseofficer eric.la.cooper@gmail.com password Cooper Eric
+../../ccd-docker/bin/ccd-add-role.sh caseworker-employment-tribunal-manchester
+../../ccd-docker/bin/ccd-add-role.sh caseworker-employment
+../../ccd-docker/bin/ccd-add-role.sh caseworker-employment-tribunal-manchester-caseofficer
+../../ccd-docker/bin/ccd-add-role.sh caseworker-employment-tribunal-manchester-casesupervisor
+../../ccd-docker/bin/ccd-import-definition.sh src/test/resources/config/CCD_EmpTrib_Manc_v1.0.09.xlsx
