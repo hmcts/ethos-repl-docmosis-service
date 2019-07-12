218bcdac4883d99fcef6ba45ef09355683ef7440
kiran <kiran.pannala@hmcts.net>
2019-07-12 15:18:11 +0100

    Added component/functional test classes for Bulk end-points

---
+++ src/test/functional/java/uk/gov/hmcts/ethos/replacement/functional/bulk/UpdateCaseBulkComponentTest.java
@@ -0,0 +1,41 @@
+package uk.gov.hmcts.ethos.replacement.functional.bulk;
+
+import net.serenitybdd.junit.runners.SerenityRunner;
+import net.thucydides.core.annotations.WithTag;
+import net.thucydides.core.annotations.WithTags;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import org.junit.runner.RunWith;
+import uk.gov.hmcts.ethos.replacement.functional.ComponentTest;
+import uk.gov.hmcts.ethos.replacement.functional.util.Constants;
+import uk.gov.hmcts.ethos.replacement.functional.util.TestUtil;
+
+import java.io.IOException;
+
+@Category(ComponentTest.class)
+@RunWith(SerenityRunner.class)
+@WithTags({
+        @WithTag("ComponentTest"),
+        @WithTag("FunctionalTest")
+})
+public class UpdateCaseBulkComponentTest {
+    private TestUtil testUtil;
+
+    @Before
+    public void setUp() {
+        testUtil = new TestUtil();
+    }
+
+    @Test
+    @WithTag("SmokeTest")
+    public void update_bulk_test1() throws IOException {
+        testUtil.executeUpdateBulkCaseTest(false, Constants.TEST_DATA_CASE1);
+    }
+
+
+    @After
+    public void tearDown() {
+    }
+}
