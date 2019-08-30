45c51a69a63f0e05d0e6b0e18a106288a3df5fe8
Javier Carretero <javi_1986@hotmail.com>
2019-08-30 16:17:55 +0100

    Updated latest config

--- src/main/java/uk/gov/hmcts/ethos/replacement/docmosis/model/ccd/types/RespondentSumType.java
+++ src/main/java/uk/gov/hmcts/ethos/replacement/docmosis/model/ccd/types/RespondentSumType.java
@@ -9,6 +9,28 @@ import uk.gov.hmcts.ethos.replacement.docmosis.model.ccd.Address;
 @Data
 public class RespondentSumType {
 
+    @JsonProperty("response_status")
+    private String responseStatus;
+    @JsonProperty("responseToClaim")
+    private String responseToClaim;
+    @JsonProperty("rejection_reason")
+    private String rejectionReason;
+    @JsonProperty("rejection_reason_other")
+    private String rejectionReasonOther;
+    @JsonProperty("responseNotes")
+    private String responseNotes;
+    @JsonProperty("response_referred_to_judge")
+    private String response_ReferredToJudge;
+    @JsonProperty("response_returned_from_judge")
+    private String responseReturnedFromJudge;
+    @JsonProperty("response_out_of_time?")
+    private String responseOutOfTime;
+    @JsonProperty("response_not_on_prescribed_form?")
+    private String responseNotOnPrescribedForm;
+    @JsonProperty("response_required_info_absent?")
+    private String responseRequiredInfoAbsent;
+    @JsonProperty("responseReceivedDate?")
+    private String responseReceivedDate;
     @JsonProperty("respondent_name")
     private String respondentName;
     @JsonProperty("respondent_ACAS_question")
