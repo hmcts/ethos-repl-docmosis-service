a7ee6abbd1af0d068b9014ee595b53a6800310ca
Javier Carretero <javi_1986@hotmail.com>
2019-09-06 13:52:30 +0100

    Removed sequence

--- src/main/java/uk/gov/hmcts/ethos/replacement/docmosis/controllers/CaseActionsForCaseWorkerController.java
+++ src/main/java/uk/gov/hmcts/ethos/replacement/docmosis/controllers/CaseActionsForCaseWorkerController.java
@@ -167,6 +167,7 @@ public class CaseActionsForCaseWorkerController {
             caseData = defaultValuesReaderService.getCaseData(ccdRequest.getCaseDetails().getCaseData(), defaultValues);
             log.info("Post Default caseData: " + caseData);
             Reference reference = referenceService.createReference(ccdRequest.getCaseDetails().getCaseId());
+            referenceService.getReference();
             log.info("Reference generated: " + reference.getId());
             //caseData.setEthosCaseReference(String.valueOf(reference.getId()));
         } else {
