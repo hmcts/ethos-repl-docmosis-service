7d175267b6da60fa0237e86b8678404032ff44de
Javier Carretero <javi_1986@hotmail.com>
2019-09-13 11:32:32 +0100

    Multiple reference numbers

---
+++ src/main/java/uk/gov/hmcts/ethos/replacement/docmosis/domain/repository/MultipleRefWalesRepository.java
@@ -0,0 +1,8 @@
+package uk.gov.hmcts.ethos.replacement.docmosis.domain.repository;
+
+import org.springframework.stereotype.Repository;
+import uk.gov.hmcts.ethos.replacement.docmosis.domain.MultipleReferenceWales;
+
+@Repository
+public interface MultipleRefWalesRepository extends MultipleRefRepository<MultipleReferenceWales> {
+}
