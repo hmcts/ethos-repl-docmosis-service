c645542c793352548c528530e3e6b85dbd707c22
Javier Carretero <javi_1986@hotmail.com>
2019-10-04 10:29:26 +0200

    Creation SubMultiple Ref number

---
+++ src/main/java/uk/gov/hmcts/ethos/replacement/docmosis/domain/repository/SubMultipleRefWalesRepository.java
@@ -0,0 +1,8 @@
+package uk.gov.hmcts.ethos.replacement.docmosis.domain.repository;
+
+import org.springframework.stereotype.Repository;
+import uk.gov.hmcts.ethos.replacement.docmosis.domain.SubMultipleReferenceWales;
+
+@Repository
+public interface SubMultipleRefWalesRepository extends SubMultipleRefRepository<SubMultipleReferenceWales> {
+}
