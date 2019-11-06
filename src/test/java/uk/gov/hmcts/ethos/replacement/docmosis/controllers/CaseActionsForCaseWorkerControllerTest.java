0d74b1a112559a8f724ecd59896b3ca6cbf9709e
Javier Carretero <javi_1986@hotmail.com>
2019-11-06 15:20:07 +0100

    Amend case state

--- src/test/java/uk/gov/hmcts/ethos/replacement/docmosis/controllers/CaseActionsForCaseWorkerControllerTest.java
+++ src/test/java/uk/gov/hmcts/ethos/replacement/docmosis/controllers/CaseActionsForCaseWorkerControllerTest.java
@@ -52,6 +52,7 @@ public class CaseActionsForCaseWorkerControllerTest {
     private static final String POST_DEFAULT_VALUES_URL = "/postDefaultValues";
     private static final String PRE_ACCEPT_CASE_URL = "/preAcceptCase";
     private static final String AMEND_CASE_DETAILS_URL = "/amendCaseDetails";
+    private static final String AMEND_CASE_STATE_URL = "/amendCaseState";
 
     @Autowired
     private WebApplicationContext applicationContext;
@@ -401,4 +402,24 @@ public class CaseActionsForCaseWorkerControllerTest {
                 .andExpect(status().isInternalServerError());
     }
 
+    @Test
+    public void amendCaseState() throws Exception {
+        mvc.perform(post(AMEND_CASE_STATE_URL)
+                .content(requestContent.toString())
+                .header("Authorization", AUTH_TOKEN)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.data", notNullValue()))
+                .andExpect(jsonPath("$.errors", nullValue()))
+                .andExpect(jsonPath("$.warnings", nullValue()));
+    }
+
+    @Test
+    public void amendCaseStateError400() throws Exception {
+        mvc.perform(post(AMEND_CASE_STATE_URL)
+                .content("error")
+                .header("Authorization", AUTH_TOKEN)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest());
+    }
 }
