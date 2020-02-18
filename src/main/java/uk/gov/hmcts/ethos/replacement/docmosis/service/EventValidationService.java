51e8a00f73850f156f31476e7e9ee26c7db65daf
Rabab Rabab <rabab.rabab@hmcts.net>
2020-02-18 14:05:53 +0000

    Populate Target Hearing date after on Receipt date changes

--- src/main/java/uk/gov/hmcts/ethos/replacement/docmosis/service/EventValidationService.java
+++ src/main/java/uk/gov/hmcts/ethos/replacement/docmosis/service/EventValidationService.java
@@ -2,6 +2,7 @@ package uk.gov.hmcts.ethos.replacement.docmosis.service;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import uk.gov.hmcts.ethos.replacement.docmosis.model.ccd.CaseData;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
@@ -13,14 +14,17 @@ import static uk.gov.hmcts.ethos.replacement.docmosis.model.helper.Constants.*;
 @Service("eventValidationService")
 public class EventValidationService {
 
-    public List<String> validateReceiptDate(String receiptDate) {
+    public List<String> validateReceiptDate(CaseData caseData) {
 
         List<String> errors = new ArrayList<>();
 
-        LocalDate dateOfReceipt = LocalDate.parse(receiptDate);
+        LocalDate dateOfReceipt = LocalDate.parse(caseData.getReceiptDate());
 
         if (dateOfReceipt.isAfter(LocalDate.now())) {
             errors.add(FUTURE_RECEIPT_DATE_ERROR_MESSAGE);
+        }
+        else{
+            caseData.setTargetHearingDate(dateOfReceipt.plusDays(TARGET_HEARING_DATE_INCREMENT).toString());
         }
 
         return errors;
