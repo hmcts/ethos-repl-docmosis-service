8e722fd8ba6428ebeac84b8d4e89f5447a0f8196
Javier Carretero <javi_1986@hotmail.com>
2020-04-29 10:12:03 +0100

    Integer to entity

--- src/main/java/uk/gov/hmcts/ethos/replacement/docmosis/domain/SubMultipleReference.java
+++ src/main/java/uk/gov/hmcts/ethos/replacement/docmosis/domain/SubMultipleReference.java
@@ -3,10 +3,7 @@ package uk.gov.hmcts.ethos.replacement.docmosis.domain;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.MappedSuperclass;
+import javax.persistence.*;
 
 @MappedSuperclass
 @Data
@@ -17,6 +14,7 @@ public class SubMultipleReference {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
-    protected int multref;
-    protected int submultref;
+    @Column(nullable = false)
+    protected Integer multref;
+    protected Integer submultref;
 }
