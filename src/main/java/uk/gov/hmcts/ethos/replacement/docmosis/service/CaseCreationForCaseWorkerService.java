0c0289901c108a947a57790e7279bfb1ec68681f
Javier Carretero <javi_1986@hotmail.com>
2020-04-30 15:05:00 +0100

    Fixed multiple ref

--- src/main/java/uk/gov/hmcts/ethos/replacement/docmosis/service/CaseCreationForCaseWorkerService.java
+++ src/main/java/uk/gov/hmcts/ethos/replacement/docmosis/service/CaseCreationForCaseWorkerService.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import uk.gov.hmcts.ethos.replacement.docmosis.client.CcdClient;
 import uk.gov.hmcts.ethos.replacement.docmosis.exceptions.CaseCreationException;
+import uk.gov.hmcts.ethos.replacement.docmosis.helpers.BulkHelper;
 import uk.gov.hmcts.ethos.replacement.docmosis.model.ccd.CCDRequest;
 import uk.gov.hmcts.ethos.replacement.docmosis.model.ccd.CaseData;
 import uk.gov.hmcts.ethos.replacement.docmosis.model.ccd.CaseDetails;
@@ -46,7 +47,7 @@ public class CaseCreationForCaseWorkerService {
             log.info("Count: " + Integer.parseInt(caseData.getCaseRefNumberCount()));
             caseData.setStartCaseRefNumber(singleReferenceService.createReference(ccdRequest.getCaseDetails().getCaseTypeId(),
                     Integer.parseInt(caseData.getCaseRefNumberCount())));
-            caseData.setMultipleRefNumber(multipleReferenceService.createReference(ccdRequest.getCaseDetails().getCaseTypeId(), 1));
+            caseData.setMultipleRefNumber(multipleReferenceService.createReference(BulkHelper.getBulkCaseTypeId(ccdRequest.getCaseDetails().getCaseTypeId()), 1));
             log.info("StartCaseRefNumber: " + caseData.getStartCaseRefNumber());
             log.info("MultipleRefNumber: " + caseData.getMultipleRefNumber());
         }
