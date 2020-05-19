a09ba2f02a5c5b18b0815031834978af3a7bfdcd
Rabab Rabab <rabab.rabab@hmcts.net>
2020-05-19 18:37:00 +0100

    added validation for the ET3  response received date

--- src/main/java/uk/gov/hmcts/ethos/replacement/docmosis/service/EventValidationService.java
+++ src/main/java/uk/gov/hmcts/ethos/replacement/docmosis/service/EventValidationService.java
@@ -21,75 +21,45 @@ import static uk.gov.hmcts.ethos.replacement.docmosis.helpers.Helper.*;
 public class EventValidationService {
 
     public List<String> validateReceiptDate(CaseData caseData) {
-
         List<String> errors = new ArrayList<>();
-
         LocalDate dateOfReceipt = LocalDate.parse(caseData.getReceiptDate());
-
         if (dateOfReceipt.isAfter(LocalDate.now())) {
             errors.add(FUTURE_RECEIPT_DATE_ERROR_MESSAGE);
         }
         else{
             caseData.setTargetHearingDate(dateOfReceipt.plusDays(TARGET_HEARING_DATE_INCREMENT).toString());
         }
-
         return errors;
     }
 
     public List<String> validateActiveRespondents(CaseData caseData) {
-
         List<String> errors = new ArrayList<>();
-
         if(getActiveRespondents(caseData).isEmpty()) {
             errors.add(EMPTY_RESPONDENT_COLLECTION_ERROR_MESSAGE);
         }
-
         return errors;
     }
 
-    public List<String> validateReturnedFromJudgeDate(CaseData caseData) {
-
+    public List<String> validateET3ResponseFields(CaseData caseData) {
         List<String> errors = new ArrayList<>();
-
         if (caseData.getRespondentCollection() != null && !caseData.getRespondentCollection().isEmpty()) {
-
             ListIterator<RespondentSumTypeItem> itr = caseData.getRespondentCollection().listIterator();
-
             while (itr.hasNext()) {
-
                 int index = itr.nextIndex() + 1;
-
                 RespondentSumType respondentSumType = itr.next().getValue();
-
-                if (respondentSumType.getResponseReturnedFromJudge() != null && respondentSumType.getResponse_ReferredToJudge() != null) {
-
-                    LocalDate responseReferredToJudge = LocalDate.parse(respondentSumType.getResponse_ReferredToJudge());
-                    LocalDate responseReturnedFromJudge = LocalDate.parse(respondentSumType.getResponseReturnedFromJudge());
-
-                    String respondentName = respondentSumType.getRespondentName() != null ? respondentSumType.getRespondentName() : "missing name";
-
-                    if (responseReturnedFromJudge.isBefore(responseReferredToJudge)) {
-                        errors.add(EARLY_DATE_RETURNED_FROM_JUDGE_ERROR_MESSAGE + " for respondent " + index + " (" + respondentName + ")");
-                    }
-                }
+                validateResponseReceivedDateDate(respondentSumType, errors, index);
+                validateResponseReturnedFromJudgeDate(respondentSumType, errors, index);
             }
         }
-
         return errors;
     }
 
     public List<String> validateHearingNumber(CaseData caseData) {
-
         List<String> errors = new ArrayList<>();
-
         String correspondenceHearingNumber = getCorrespondenceHearingNumber(caseData);
-
         if(correspondenceHearingNumber != null) {
-
             if (caseData.getHearingCollection() != null && !caseData.getHearingCollection().isEmpty()) {
-
                 HearingType hearingType = getHearingByNumber(caseData.getHearingCollection(), correspondenceHearingNumber);
-
                 if (hearingType.getHearingNumber() == null || !hearingType.getHearingNumber().equals(correspondenceHearingNumber)) {
                     errors.add(HEARING_NUMBER_MISMATCH_ERROR_MESSAGE);
                 }
@@ -98,59 +68,66 @@ public class EventValidationService {
                 errors.add(EMPTY_HEARING_COLLECTION_ERROR_MESSAGE);
             }
         }
-
         return errors;
     }
 
     public List<String> validateJurisdictionCodes(CaseData caseData) {
-
         List<String> errors = new ArrayList<>();
-
         if (caseData.getJurCodesCollection() != null && !caseData.getJurCodesCollection().isEmpty()) {
-
             int counter = 0;
             Set<String> uniqueCodes = new HashSet<>();
             List<String> duplicateCodes = new ArrayList<>();
-
             for (JurCodesTypeItem jurCodesTypeItem : caseData.getJurCodesCollection()) {
                 counter++;
                 String code = jurCodesTypeItem.getValue().getJuridictionCodesList();
                 if(!uniqueCodes.add(code)){
                     duplicateCodes.add(" \"" + code + "\" " + "in Jurisdiction" + " " + counter + " ");
                 }
-
             }
-
             if(!duplicateCodes.isEmpty()) {
                 errors.add(DUPLICATE_JURISDICTION_CODE_ERROR_MESSAGE + StringUtils.join(duplicateCodes, '-'));
             }
         }
-
         return errors;
     }
 
     public List<String> validateJurisdictionOutcome(CaseData caseData) {
-
         List<String> errors = new ArrayList<>();
-
         if (caseData.getJurCodesCollection() != null && !caseData.getJurCodesCollection().isEmpty()) {
-
             ListIterator<JurCodesTypeItem> itr = caseData.getJurCodesCollection().listIterator();
-
             while (itr.hasNext()) {
                 JurCodesType jurCodesType = itr.next().getValue();
-
                 if (jurCodesType.getJudgmentOutcome() == null) {
                     errors.add(MISSING_JURISDICTION_OUTCOME_ERROR_MESSAGE);
                     break;
                 }
             }
-
         }
         else {
             errors.add(MISSING_JURISDICTION_OUTCOME_ERROR_MESSAGE);
         }
-
         return errors;
     }
+
+    private void validateResponseReturnedFromJudgeDate(RespondentSumType respondentSumType, List<String> errors, int index) {
+        if (respondentSumType.getResponse_ReferredToJudge() != null && respondentSumType.getResponseReturnedFromJudge() != null) {
+            LocalDate responseReferredToJudge = LocalDate.parse(respondentSumType.getResponse_ReferredToJudge());
+            LocalDate responseReturnedFromJudge = LocalDate.parse(respondentSumType.getResponseReturnedFromJudge());
+            if (responseReturnedFromJudge.isBefore(responseReferredToJudge)) {
+                String respondentName = respondentSumType.getRespondentName() != null ? respondentSumType.getRespondentName() : "missing name";
+                errors.add(EARLY_DATE_RETURNED_FROM_JUDGE_ERROR_MESSAGE + " for respondent " + index + " (" + respondentName + ")");
+            }
+        }
+    }
+
+    private void validateResponseReceivedDateDate(RespondentSumType respondentSumType, List<String> errors, int index) {
+        if (respondentSumType.getResponseReceivedDate() != null) {
+            LocalDate responseReceivedDate = LocalDate.parse(respondentSumType.getResponseReceivedDate());
+            if(responseReceivedDate.isAfter(LocalDate.now())) {
+                String respondentName = respondentSumType.getRespondentName() != null ? respondentSumType.getRespondentName() : "missing name";
+                errors.add(FUTURE_RESPONSE_RECEIVED_DATE_ERROR_MESSAGE + " for respondent " + index + " (" + respondentName + ")");
+            }
+        }
+    }
+
 }
