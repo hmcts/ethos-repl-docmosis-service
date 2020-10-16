0498cc7d399a04f8462f780a0824af2162892a40
Javier Carretero <javi_1986@hotmail.com>
2020-10-16 09:26:57 +0100

    Bumped common version

--- src/test/java/uk/gov/hmcts/ethos/replacement/docmosis/service/DefaultValuesReaderServiceTest.java
+++ src/test/java/uk/gov/hmcts/ethos/replacement/docmosis/service/DefaultValuesReaderServiceTest.java
@@ -434,8 +434,8 @@ public class DefaultValuesReaderServiceTest {
                 "respondentACASNo=null, respondentAddress=Line1, PostCode, respondentPhone1=null, respondentPhone2=null, respondentEmail=null, " +
                 "respondentContactPreference=null, responseStruckOut=null, responseStruckOutDate=null, responseStruckOutChairman=null, responseStruckOutReason=null, " +
                 "responseRespondentAddress=null, responseRespondentPhone1=null, responseRespondentPhone2=null, responseRespondentEmail=null, " +
-                "responseRespondentContactPreference=null, responseReceivedDate=null, responseRespondentNameQuestion=null, responseRespondentName=null, " +
-                "responseCounterClaim=null))], repCollection=null, positionType=Manually Created, fileLocation=null, fileLocationGlasgow=null, " +
+                "responseRespondentContactPreference=null, responseReceived=null, responseReceivedDate=null, responseRespondentNameQuestion=null, responseRespondentName=null, " +
+                "responseContinue=null, responseCounterClaim=null))], repCollection=null, positionType=Manually Created, fileLocation=null, fileLocationGlasgow=null, " +
                 "fileLocationAberdeen=null, fileLocationDundee=null, fileLocationEdinburgh=null, hearingCollection=null, depositCollection=null, " +
                 "judgementCollection=null, judgementDetailsCollection=null, costsCollection=null, jurCodesCollection=null, broughtForwardCollection=null, " +
                 "clerkResponsible=null, userLocation=null, documentCollection=null, backFromJudge=null, additionalCaseInfoType=null, correspondenceScotType=null, " +
