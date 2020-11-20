409a626cd1bc8f25f78d4affef270ddc1b1c2e19
Javier Carretero <javi_1986@hotmail.com>
2020-11-20 10:26:47 +0000

    Multi thread for schedules

--- src/main/java/uk/gov/hmcts/ethos/replacement/docmosis/service/excel/MultipleScheduleService.java
+++ src/main/java/uk/gov/hmcts/ethos/replacement/docmosis/service/excel/MultipleScheduleService.java
@@ -5,16 +5,17 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import uk.gov.hmcts.ecm.common.model.ccd.DocumentInfo;
-import uk.gov.hmcts.ecm.common.model.ccd.SubmitEvent;
 import uk.gov.hmcts.ecm.common.model.helper.SchedulePayload;
 import uk.gov.hmcts.ecm.common.model.multiples.MultipleDetails;
 import uk.gov.hmcts.ethos.replacement.docmosis.helpers.FilterExcelType;
 import uk.gov.hmcts.ethos.replacement.docmosis.helpers.MultiplesHelper;
 import uk.gov.hmcts.ethos.replacement.docmosis.helpers.MultiplesScheduleHelper;
+import uk.gov.hmcts.ethos.replacement.docmosis.tasks.ScheduleCallable;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.TreeMap;
+import java.util.concurrent.*;
 
 @Slf4j
 @Service("multipleScheduleService")
@@ -25,6 +26,7 @@ public class MultipleScheduleService {
     private final ExcelDocManagementService excelDocManagementService;
 
     public static final int ES_PARTITION_SIZE = 500;
+    public static final int THREAD_NUMBER = 100;
 
     @Autowired
     public MultipleScheduleService(ExcelReadingService excelReadingService,
@@ -53,7 +55,7 @@ public class MultipleScheduleService {
 
         List<SchedulePayload> schedulePayloads =
                 getSchedulePayloadCollection(userToken, multipleDetails.getCaseTypeId(),
-                        getCaseIdCollectionFromFilter(multipleObjects, filterExcelType));
+                        getCaseIdCollectionFromFilter(multipleObjects, filterExcelType), errors);
 
         log.info("Generate schedule");
 
@@ -81,24 +83,41 @@ public class MultipleScheduleService {
 
     }
 
-    private List<SchedulePayload> getSchedulePayloadCollection(String userToken, String caseTypeId, List<String> caseIdCollection) {
+    private List<SchedulePayload> getSchedulePayloadCollection(String userToken, String caseTypeId, List<String> caseIdCollection, List<String> errors) {
 
-        List<SchedulePayload> schedulePayloads = new ArrayList<>();
+        ExecutorService executor = Executors.newFixedThreadPool(THREAD_NUMBER);
+
+        List<Future<List<SchedulePayload>>> resultList = new ArrayList<>();
 
         for (List<String> partitionCaseIds : Lists.partition(caseIdCollection, ES_PARTITION_SIZE)) {
 
-            List<SubmitEvent> submitEvents = singleCasesReadingService.retrieveSingleCases(userToken,
-                    caseTypeId, partitionCaseIds);
+            ScheduleCallable scheduleCallable = new ScheduleCallable(singleCasesReadingService, userToken, caseTypeId, partitionCaseIds);
 
-            for (SubmitEvent submitEvent : submitEvents) {
+            resultList.add(executor.submit(scheduleCallable));
 
-                schedulePayloads.add(MultiplesScheduleHelper.getSchedulePayloadFromSubmitEvent(submitEvent));
+        }
+
+        List<SchedulePayload> result = new ArrayList<>();
+
+        for (Future<List<SchedulePayload>> fut : resultList){
+
+            try {
+
+                result.addAll(fut.get());
+
+            } catch (InterruptedException | ExecutionException e) {
+
+                log.error("Error Generating Schedules");
+
+                errors.add("Error Generating Schedules");
 
             }
 
         }
 
-        return schedulePayloads;
+        executor.shutdown();
+
+        return result;
 
     }
 
