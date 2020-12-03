fa83548b04a1fef3a74634c30cf1f94b7381104f
Javier Carretero <javi_1986@hotmail.com>
2020-12-03 11:24:29 +0000

    Bumped common

--- src/main/java/uk/gov/hmcts/ethos/replacement/docmosis/service/excel/SingleCasesReadingService.java
+++ src/main/java/uk/gov/hmcts/ethos/replacement/docmosis/service/excel/SingleCasesReadingService.java
@@ -60,15 +60,13 @@ public class SingleCasesReadingService {
                     UtilHelper.getCaseTypeId(multipleCaseTypeId),
                     caseIds));
 
-            log.info("SchedulePayloadEvents: " + schedulePayloadEvents);
+            log.info("SchedulePayloadEventsList: " + schedulePayloadEvents);
 
         } catch (Exception ex) {
 
             log.error("Error retrieving schedule cases");
 
             log.error(ex.getMessage(), ex);
-
-            log.error(Arrays.toString(ex.getStackTrace()));
 
         }
 
