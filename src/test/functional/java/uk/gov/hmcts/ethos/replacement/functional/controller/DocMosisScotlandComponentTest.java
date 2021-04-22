d59280ea026bc531d2bfc239088eead8269c5559
hsjhita <harpreet.jhita@justice.gov.uk>
2021-04-22 16:13:48 +0100

    New tests

--- src/test/functional/java/uk/gov/hmcts/ethos/replacement/functional/controller/DocMosisScotlandComponentTest.java
+++ src/test/functional/java/uk/gov/hmcts/ethos/replacement/functional/controller/DocMosisScotlandComponentTest.java
@@ -1,14 +1,20 @@
 package uk.gov.hmcts.ethos.replacement.functional.controller;
 
 import io.restassured.RestAssured;
+import io.restassured.config.RestAssuredConfig;
+import io.restassured.config.SSLConfig;
 import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
 import net.serenitybdd.junit.runners.SerenityRunner;
+import net.serenitybdd.rest.SerenityRest;
 import org.apache.commons.io.FileUtils;
 import org.apache.http.HttpHeaders;
 import org.apache.http.HttpStatus;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
+import org.junit.jupiter.api.Assertions;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.test.context.TestPropertySource;
@@ -27,7 +33,8 @@ import static io.restassured.RestAssured.useRelaxedHTTPSValidation;
 
 @RunWith(SerenityRunner.class)
 @TestPropertySource(locations = "classpath:config/application.properties")
-public class PreDefaultComponentFunctionalTest {
+public class DocMosisScotlandComponentTest {
+
     @Value("${test-url}")
     private String testUrl;
 
@@ -42,43 +49,24 @@ public class PreDefaultComponentFunctionalTest {
 
     @Test
     @Category(FunctionalTest.class)
-    public void claimaintIndividualEng() throws IOException {
-        CCDRequest ccdRequest = getCcdRequest("1", "1", false, new File(Constants.TEST_DATA_PRE_DEFAULT1));
-        RestAssured.given()
-            .header(HttpHeaders.AUTHORIZATION, AUTH_TOKEN)
-            .header(HttpHeaders.CONTENT_TYPE, ContentType.JSON)
-            .body(ccdRequest)
-            .post("/preDefaultValues")
-            .then()
-            .statusCode(HttpStatus.SC_FORBIDDEN); // Should be 200
+    public void generateDocumentDundee() throws IOException {
+        CCDRequest ccdRequest = getCcdRequest("1", "", true, new File(Constants.TEST_DATA_SCOT_DUNDEE_CASE1));
+        Response response = getResponse(ccdRequest, "/amendCaseDetails");
+        Assertions.assertEquals(HttpStatus.SC_FORBIDDEN, response.getStatusCode()); // Should be 200 but getting 403
+
     }
 
-    @Test
-    @Category(FunctionalTest.class)
-    public void preDefaultNoPayload() {
-        CCDRequest ccdRequest = new CCDRequest();
-        RestAssured.given()
-            .header(HttpHeaders.AUTHORIZATION, AUTH_TOKEN)
-            .header(HttpHeaders.CONTENT_TYPE, ContentType.JSON)
-            .body(ccdRequest)
-            .post("/preDefaultValues")
-            .then()
-            .statusCode(403);
+    private Response getResponse(CCDRequest ccdRequest, String uri) {
+        return RestAssured.given()
+                .header(HttpHeaders.AUTHORIZATION, AUTH_TOKEN)
+                .header(HttpHeaders.CONTENT_TYPE, ContentType.JSON)
+                .body(ccdRequest)
+                .post(uri);
     }
 
-    @Test
-    @Category(FunctionalTest.class)
-    public void invokePreDefaultAsGet() {
-        RestAssured.given()
-            .header(HttpHeaders.AUTHORIZATION, AUTH_TOKEN)
-            .header(HttpHeaders.CONTENT_TYPE, ContentType.JSON)
-            .get("/preDefaultValues")
-            .then()
-            .statusCode(405);
-    }
-
-    public CCDRequest getCcdRequest(String topLevel, String childLevel, boolean isScotland, File testData) throws IOException {
-        String payload = FileUtils.readFileToString(testData,"UTF-8");
+    private CCDRequest getCcdRequest(String topLevel, String childLevel, boolean isScotland, File testData) throws IOException {
+        String payload = FileUtils.readFileToString(testData, "UTF-8");
         return JsonUtil.getCaseDetails(payload, topLevel, childLevel, isScotland);
     }
+
 }
