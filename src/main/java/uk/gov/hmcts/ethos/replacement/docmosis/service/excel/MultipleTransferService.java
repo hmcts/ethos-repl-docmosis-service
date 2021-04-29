f74e716c71e12ead64450505f1cda4cc9e4ee3f0
MSancaktutar <mutlu.sancaktutar@hmcts.net>
2021-04-29 14:05:39 +0100

    Revert "R5.0.8"

--- src/main/java/uk/gov/hmcts/ethos/replacement/docmosis/service/excel/MultipleTransferService.java
+++ src/main/java/uk/gov/hmcts/ethos/replacement/docmosis/service/excel/MultipleTransferService.java
@@ -99,7 +99,7 @@ public class MultipleTransferService {
             String oldCaseTypeId = multipleDetails.getCaseData().getLinkedMultipleCT();
             String multipleReference = multipleDetails.getCaseData().getMultipleReference();
 
-            log.info("Retrieve the old multiple data");
+            log.info("Retrieve the old multiple info");
 
             SubmitMultipleEvent oldSubmitMultipleEvent = multipleCasesReadingService.retrieveMultipleCasesWithRetries(
                     userToken,
@@ -120,7 +120,6 @@ public class MultipleTransferService {
                     String.valueOf(oldSubmitMultipleEvent.getCaseId()),
                     multipleReference));
 
-            multipleDetails.getCaseData().setReasonForCT(oldSubmitMultipleEvent.getCaseData().getReasonForCT());
         }
 
     }
