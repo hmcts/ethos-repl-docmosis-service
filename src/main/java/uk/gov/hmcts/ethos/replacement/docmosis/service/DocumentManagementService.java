62ff656d2227a74e6e2ebfbd3ccd0cea32c0d24c
hsjhita <harpreet.jhita@justice.gov.uk>
2021-09-22 16:03:58 +0100

    Readded old stuff

--- src/main/java/uk/gov/hmcts/ethos/replacement/docmosis/service/DocumentManagementService.java
+++ src/main/java/uk/gov/hmcts/ethos/replacement/docmosis/service/DocumentManagementService.java
@@ -15,8 +15,11 @@ import org.springframework.web.multipart.MultipartFile;
 import uk.gov.hmcts.ecm.common.exceptions.DocumentManagementException;
 import uk.gov.hmcts.ecm.common.model.ccd.UploadedDocument;
 import uk.gov.hmcts.reform.authorisation.generators.AuthTokenGenerator;
+import uk.gov.hmcts.reform.ccd.document.am.feign.CaseDocumentClient;
 import uk.gov.hmcts.reform.ccd.document.am.feign.CaseDocumentClientApi;
 import uk.gov.hmcts.reform.ccd.document.am.model.DocumentUploadRequest;
+import uk.gov.hmcts.reform.document.DocumentDownloadClientApi;
+import uk.gov.hmcts.reform.document.DocumentUploadClientApi;
 import uk.gov.hmcts.reform.document.utils.InMemoryMultipartFile;
 
 import java.net.URI;
@@ -35,7 +38,9 @@ public class DocumentManagementService {
     public static final String APPLICATION_DOCX_VALUE =
             "application/vnd.openxmlformats-officedocument.wordprocessingml.document";
     private final AuthTokenGenerator authTokenGenerator;
-
+    private final DocumentUploadClientApi documentUploadClient;
+    private final UserService userService;
+    private final DocumentDownloadClientApi documentDownloadClientApi;
     private final CaseDocumentClientApi caseDocumentClientApi;
 
     @Value("${ccd_gateway_base_url}")
@@ -45,13 +50,47 @@ public class DocumentManagementService {
 
     @Autowired
     public DocumentManagementService(AuthTokenGenerator authTokenGenerator,
-                                     CaseDocumentClientApi caseDocumentClientApi) {
+                                     CaseDocumentClientApi caseDocumentClientApi,
+                                     DocumentUploadClientApi documentUploadClient,
+                                     UserService userService,
+                                     DocumentDownloadClientApi documentDownloadClientApi) {
         this.authTokenGenerator = authTokenGenerator;
         this.caseDocumentClientApi = caseDocumentClientApi;
+        this.documentUploadClient = documentUploadClient;
+        this.userService = userService;
+        this.documentDownloadClientApi = documentDownloadClientApi;
     }
 
     @Retryable(value = {DocumentManagementException.class}, backoff = @Backoff(delay = 200))
     public URI uploadDocument(String authToken, byte[] byteArray, String outputFileName, String type) {
+//        try {
+//            MultipartFile file = new InMemoryMultipartFile(FILES_NAME, outputFileName, type, byteArray);
+//            var user = userService.getUserDetails(authToken);
+//            UploadResponse response = documentUploadClient.upload(
+//                    authToken,
+//                    authTokenGenerator.generate(),
+//                    user.getUid(),
+//                    new ArrayList<>(singletonList("caseworker-employment")),
+//                    Classification.PUBLIC,
+//                    singletonList(file)
+//            );
+//            var document = response.getEmbedded().getDocuments().stream()
+//                    .findFirst()
+//
+//                    .orElseThrow(() ->
+//                            new DocumentManagementException("Document management failed uploading file"
+//                                    + OUTPUT_FILE_NAME));
+//
+//            log.info("Uploaded document successful");
+//            for(int i = 0; i<10; i++){
+//                log.info("-------------------------");
+//            }
+//            return URI.create(document.links.self.href);
+//        } catch (Exception ex) {
+//            log.info("Exception: " + ex.getMessage());
+//            throw new DocumentManagementException(String.format("Unable to upload document %s to document management",
+//                    outputFileName), ex);
+//        }
         try {
             MultipartFile file = new InMemoryMultipartFile(FILES_NAME, outputFileName, type, byteArray);
             var response = caseDocumentClientApi.uploadDocuments(
@@ -59,7 +98,7 @@ public class DocumentManagementService {
                     authTokenGenerator.generate(),
                     new DocumentUploadRequest(
                             "PUBLIC",
-                            "caseTypeId",
+                            "Watford",
                             "EMPLOYMENT",
                             singletonList(file)
                 )
@@ -77,6 +116,8 @@ public class DocumentManagementService {
             throw new DocumentManagementException(String.format("Unable to upload document %s to document management",
                     outputFileName), ex);
         }
+
+
     }
 
 
@@ -86,6 +127,9 @@ public class DocumentManagementService {
                 authTokenGenerator.generate(),
                 UUID.fromString(getDownloadUrl(urlString)));
         if (HttpStatus.OK.equals(responseEntity.getStatusCode())) {
+            for(int i = 0; i<10; i++){
+                log.info("----Response OK-----");
+            }
             return UploadedDocument.builder()
                     .content(responseEntity.getBody())
                     .name(Objects.requireNonNull(responseEntity.getHeaders().get("originalfilename")).get(0))
@@ -95,10 +139,32 @@ public class DocumentManagementService {
             throw new IllegalStateException("Cannot download document that is stored in CCD got "
                     + "[" + responseEntity.getStatusCode() + "] " + responseEntity.getBody());
         }
+
+//        var user = userService.getUserDetails(authToken);
+//        ResponseEntity<Resource> response = documentDownloadClientApi.downloadBinary(
+//                authToken,
+//                authTokenGenerator.generate(),
+//                String.join(",", user.getRoles()),
+//                user.getUid(),
+//                getDownloadUrl(urlString)
+//        );
+//        if (HttpStatus.OK.equals(response.getStatusCode())) {
+//            for(int i = 0; i<10; i++){
+//                log.info("----Response OK-----");
+//            }
+//            return UploadedDocument.builder()
+//                    .content(response.getBody())
+//                    .name(Objects.requireNonNull(response.getHeaders().get("originalfilename")).get(0))
+//                    .contentType(Objects.requireNonNull(response.getHeaders().get(HttpHeaders.CONTENT_TYPE)).get(0))
+//                    .build();
+//        } else {
+//            throw new IllegalStateException("Cannot download document that is stored in CCD got "
+//                    + "[" + response.getStatusCode() + "] " + response.getBody());
+//        }
     }
 
     private String getDownloadUrl(String urlString) {
-        var path = urlString.replace(ccdDMStoreBaseUrl+"/documents/", "");
+        var path = urlString.replace(ccdDMStoreBaseUrl, "");
         log.info(path);
         if (path.startsWith("/")) {
             return path;
