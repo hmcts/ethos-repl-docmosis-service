63f518e8a6d4c81a6cfc2e24356a8bf0a0696af0
PhaniP <phani.perla@hmcts.net>
2021-11-23 17:10:42 +0000

    e2e-tests-script update

--- bin/run-e2e-tests.sh
+++ bin/run-e2e-tests.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 set -ex
-### FOR DEMO
-
-export CCD_CASEWORKER_E2E_EMAIL='employment_service@mailinator.com'
-export CCD_CASEWORKER_E2E_PASSWORD='Nagoya0102'
-export IDAM_CLIENT_SECRET=
-export SERVICE_SECRET='DBssSOq0KKLNBf2z'
-
 
 export TESTS_FOR_ACCESSIBILITY='true'
 export E2E_OUTPUT_DIR='./functional-output/'
