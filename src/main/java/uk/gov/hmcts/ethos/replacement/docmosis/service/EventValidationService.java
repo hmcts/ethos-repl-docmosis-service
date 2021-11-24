4063d645f37c2c89eb3d8637d8ee082186c95945
hsjhita <harpreet.jhita@justice.gov.uk>
2021-11-24 15:57:47 +0000

    Update EventValidationService.java

--- src/main/java/uk/gov/hmcts/ethos/replacement/docmosis/service/EventValidationService.java
+++ src/main/java/uk/gov/hmcts/ethos/replacement/docmosis/service/EventValidationService.java
@@ -365,47 +365,57 @@ public class EventValidationService {
                     errors.add(DEPOSIT_REFUNDED_GREATER_DEPOSIT_ERROR);
                 }
 
-                if (depositTypeItem.getValue().getDynamicDepositOrderAgainst() != null) {
-                    var dynamicOrderAgainst = depositTypeItem.getValue().getDynamicDepositOrderAgainst()
-                            .getValue().getCode();
-                    if (dynamicOrderAgainst.startsWith("R:")) {
-                        depositTypeItem.getValue().setDepositOrderAgainst(RESPONDENT_TITLE);
-                    } else if (dynamicOrderAgainst.startsWith("C:")) {
-                        depositTypeItem.getValue().setDepositOrderAgainst(CLAIMANT_TITLE);
-                    } else {
-                        errors.add(UNABLE_TO_FIND_PARTY);
-                    }
-                }
-
-                if (depositTypeItem.getValue().getDynamicDepositRequestedBy() != null) {
-                    var dynamicRequestedBy = depositTypeItem.getValue().getDynamicDepositRequestedBy()
-                            .getValue().getCode();
-                    if (dynamicRequestedBy.startsWith("R:")) {
-                        depositTypeItem.getValue().setDepositRequestedBy(RESPONDENT_TITLE);
-                    } else if (dynamicRequestedBy.startsWith("C:")) {
-                        depositTypeItem.getValue().setDepositRequestedBy(CLAIMANT_TITLE);
-                    } else if (dynamicRequestedBy.equals("Tribunal")) {
-                        depositTypeItem.getValue().setDepositRequestedBy("Tribunal");
-                    } else {
-                        errors.add(UNABLE_TO_FIND_PARTY);
-                    }
-                }
-
-                if (!isNullOrEmpty(depositTypeItem.getValue().getDepositRefund())
-                        && depositTypeItem.getValue().getDynamicDepositRefundedTo() != null) {
-                    var dynamicRefundedTo = depositTypeItem.getValue().getDynamicDepositRefundedTo()
-                            .getValue().getCode();
-                    if (dynamicRefundedTo.startsWith("R: ")) {
-                        depositTypeItem.getValue().setDepositRefundedTo(RESPONDENT_TITLE);
-                    } else if (dynamicRefundedTo.startsWith("C: ")) {
-                        depositTypeItem.getValue().setDepositRefundedTo(CLAIMANT_TITLE);
-                    } else {
-                        errors.add(UNABLE_TO_FIND_PARTY);
-                    }
-                }
+                validateDepositOrderAgainst(errors, depositTypeItem);
+                validateDepositRequestedBy(errors, depositTypeItem);
+                validateDepositRefunded(errors, depositTypeItem);
             }
         }
         return errors;
+    }
+
+    private void validateDepositRefunded(List<String> errors, DepositTypeItem depositTypeItem) {
+        if (!isNullOrEmpty(depositTypeItem.getValue().getDepositRefund())
+                && depositTypeItem.getValue().getDynamicDepositRefundedTo() != null) {
+            var dynamicRefundedTo = depositTypeItem.getValue().getDynamicDepositRefundedTo()
+                    .getValue().getCode();
+            if (dynamicRefundedTo.startsWith("R: ")) {
+                depositTypeItem.getValue().setDepositRefundedTo(RESPONDENT_TITLE);
+            } else if (dynamicRefundedTo.startsWith("C: ")) {
+                depositTypeItem.getValue().setDepositRefundedTo(CLAIMANT_TITLE);
+            } else {
+                errors.add(UNABLE_TO_FIND_PARTY);
+            }
+        }
+    }
+
+    private void validateDepositRequestedBy(List<String> errors, DepositTypeItem depositTypeItem) {
+        if (depositTypeItem.getValue().getDynamicDepositRequestedBy() != null) {
+            var dynamicRequestedBy = depositTypeItem.getValue().getDynamicDepositRequestedBy()
+                    .getValue().getCode();
+            if (dynamicRequestedBy.startsWith("R:")) {
+                depositTypeItem.getValue().setDepositRequestedBy(RESPONDENT_TITLE);
+            } else if (dynamicRequestedBy.startsWith("C:")) {
+                depositTypeItem.getValue().setDepositRequestedBy(CLAIMANT_TITLE);
+            } else if (dynamicRequestedBy.equals("Tribunal")) {
+                depositTypeItem.getValue().setDepositRequestedBy("Tribunal");
+            } else {
+                errors.add(UNABLE_TO_FIND_PARTY);
+            }
+        }
+    }
+
+    private void validateDepositOrderAgainst(List<String> errors, DepositTypeItem depositTypeItem) {
+        if (depositTypeItem.getValue().getDynamicDepositOrderAgainst() != null) {
+            var dynamicOrderAgainst = depositTypeItem.getValue().getDynamicDepositOrderAgainst()
+                    .getValue().getCode();
+            if (dynamicOrderAgainst.startsWith("R:")) {
+                depositTypeItem.getValue().setDepositOrderAgainst(RESPONDENT_TITLE);
+            } else if (dynamicOrderAgainst.startsWith("C:")) {
+                depositTypeItem.getValue().setDepositOrderAgainst(CLAIMANT_TITLE);
+            } else {
+                errors.add(UNABLE_TO_FIND_PARTY);
+            }
+        }
     }
 
     public List<String> validateListingDateRange(String listingFrom, String listingTo) {
