a0c75f238de360b74fc43e15597bc3cd384ab2eb
Angela Liu-Wilkin <angela.liu-wilkin@justice.gov.uk>
2021-11-29 16:17:36 +0000

    add previous fix for zip bomb error back in

--- src/main/java/uk/gov/hmcts/ethos/replacement/docmosis/service/excel/ExcelReadingService.java
+++ src/main/java/uk/gov/hmcts/ethos/replacement/docmosis/service/excel/ExcelReadingService.java
@@ -48,6 +48,7 @@ public class ExcelReadingService {
     }
 
     public XSSFWorkbook readWorkbook(String userToken, String documentBinaryUrl) throws IOException {
+        ZipSecureFile.setMinInflateRatio(0);
         var excelInputStream =
                 excelDocManagementService.downloadExcelDocument(userToken, documentBinaryUrl);
         return new XSSFWorkbook(excelInputStream);
