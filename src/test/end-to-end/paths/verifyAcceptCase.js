5893c9ba25bee5ccf86c28d2ff722534c183e09b
PhaniP <phani.perla@hmcts.net>
2021-11-30 15:11:31 +0000

    ECM-444-Puppeteer Helper

--- src/test/end-to-end/paths/verifyAcceptCase.js
+++ src/test/end-to-end/paths/verifyAcceptCase.js
@@ -3,7 +3,7 @@ const {createCaseInCcd} = require("../helpers/ccdDataStoreApi");
 const {eventNames} = require('../pages/common/constants.js');
 let caseNumber;
 
-Feature('Create a Leeds Single Case and move it to Accept Case state');
+Feature('Create a Leeds Single Case and move to Accepted state');
 
 Scenario('Verify Accept Case', async ({I}) => {
     caseNumber = await createCaseInCcd('src/test/end-to-end/data/ccd-case-basic-data.json');
@@ -11,7 +11,6 @@ Scenario('Verify Accept Case', async ({I}) => {
     await I.authenticateWithIdam();
     await I.wait(5);
     await I.amOnPage('/case-details/' + caseNumber);
-    await I.waitForText("Next step");
     await I.chooseNextStep(eventNames.ACCEPT_CASE, 3);
     await I.acceptTheCase();
 
