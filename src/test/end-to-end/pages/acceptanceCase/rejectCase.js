7da1f246539bf252e4c6fdcd74ee7ca13ee09ca4
PhaniP <phani.perla@hmcts.net>
2021-12-01 15:06:06 +0000

    ECM-454-Verify-Rejected-Case

---
+++ src/test/end-to-end/pages/acceptanceCase/rejectCase.js
@@ -0,0 +1,16 @@
+'use strict';
+const commonConfig = require('../../data/commonConfig.json');
+
+module.exports = async function () {
+
+    const I = this;
+    await I.click('#preAcceptCase_caseAccepted_No');
+    await I.fillField('#dateRejected-day', commonConfig.caseAcceptedDay);
+    await I.fillField('#dateRejected-month', commonConfig.caseAcceptedMonth);
+    await I.fillField('#dateRejected-year', commonConfig.caseAcceptedYear);
+    await I.checkOption('#preAcceptCase_rejectReason-Not\\ on\\ Prescribed\\ Form');
+    await I.navByClick(commonConfig.continue);
+    await I.click(commonConfig.submit)
+    await I.wait(5);
+};
+
