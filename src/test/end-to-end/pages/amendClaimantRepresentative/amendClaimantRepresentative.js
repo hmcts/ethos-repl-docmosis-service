d822c791230946faf561f30fcca915be0aeea0c1
PhaniP <phani.perla@hmcts.net>
2021-12-08 12:54:51 +0000

    ECM-485-Claimant-Representative

--- src/test/end-to-end/pages/amendClaimantRepresentative/amendClaimantRepresentative.js
+++ src/test/end-to-end/pages/amendClaimantRepresentative/amendClaimantRepresentative.js
@@ -6,12 +6,11 @@ module.exports = async function () {
     const I = this;
     await I.waitForNavigationToComplete(commonConfig.claimantRepresentativeYes, 3);
     await I.fillField('#representativeClaimantType_name_of_representative', commonConfig.claimantRepresentativeName);
-    await I.fillField('#representativeClaimantType_representative_address_representative_address_postcodeInput', 'E14 3XA')
+    await I.fillField('#representativeClaimantType_representative_address_representative_address_postcodeInput', commonConfig.claimantRepresentativePostCode);
     await I.click(commonConfig.findAddressButton);
     await I.wait(3);
 
-    await I.waitForElement('//select[@id="address[addressList]"]');
-    await I.selectOption('//select[@id="address[addressList]"]', ' Flat 13, Vermeer Court, 1 Rembrandt Close, London, E14 3XA');
+    await I.selectOption('//select[@id="representativeClaimantType_representative_address_representative_address_addressList"]', commonConfig.claimantRepresentativeAddress);
     await I.wait(2);
 
     await I.navByClick(commonConfig.continue);
