f977c40d017476885d00cd2c1b20b912b5460c36
PhaniP <phani.perla@hmcts.net>
2021-12-09 10:40:22 +0000

    fixed address selectin issue

--- src/test/end-to-end/pages/amendClaimantRepresentative/amendClaimantRepresentative.js
+++ src/test/end-to-end/pages/amendClaimantRepresentative/amendClaimantRepresentative.js
@@ -1,16 +1,18 @@
 'use strict';
 const commonConfig = require('../../data/commonConfig.json');
+const testConfig = require("../../../config");
 
 module.exports = async function () {
 
     const I = this;
-    await I.waitForNavigationToComplete(commonConfig.claimantRepresentativeYes, 3);
+    await I.click(commonConfig.claimantRepresentativeYes);
     await I.fillField('#representativeClaimantType_name_of_representative', commonConfig.claimantRepresentativeName);
     await I.fillField('#representativeClaimantType_representative_address_representative_address_postcodeInput', commonConfig.claimantRepresentativePostCode);
     await I.click(commonConfig.findAddressButton);
     await I.wait(3);
 
-    await I.selectOption('//select[@id="representativeClaimantType_representative_address_representative_address_addressList"]', commonConfig.claimantRepresentativeAddress);
+    I.waitForText(commonConfig.claimantRepresentativeAddress, testConfig.TestTimeToWaitForText);
+    await I.selectOption('#representativeClaimantType_representative_address_representative_address_addressList', commonConfig.claimantRepresentativeAddress);
     await I.wait(2);
 
     await I.navByClick(commonConfig.continue);
