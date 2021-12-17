89662f463d527ce69368e08d8d5fb84e5e94353e
shravan mechineni <shravanmechineni5@gmail.com>
2021-12-17 17:59:07 +0000

    add manchester juris events tests

---
+++ src/test/end-to-end/paths/verifyManchesterCaseClaimantRepresentative.js
@@ -0,0 +1,16 @@
+const testConfig = require('./../../config');
+const {createCaseInCcd} = require("../helpers/ccdDataStoreApi");
+const {eventNames} = require('../pages/common/constants.js');
+const {acceptCaseEvent, claimantRepresentative} = require("../helpers/caseHelper");
+let caseNumber;
+
+Feature('Create A Single Case & Execute Claimant Representative...');
+
+Scenario('Verify Claimant Representative', async ({I}) => {
+    caseNumber = await createCaseInCcd('src/test/end-to-end/data/ccd-case-manchester-data.json', 'Manchester');
+    await acceptCaseEvent(I, caseNumber, eventNames.ACCEPT_CASE);
+    await claimantRepresentative(I, eventNames.CLAIMANT_REPRESENTATIVE);
+
+}).tag('@e2e')
+    .tag('@manchester')
+    .retry(testConfig.TestRetryScenarios);
