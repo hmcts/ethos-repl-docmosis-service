6268d22300914142c5ef7710911751be6c317330
gauravchawla <gaurav-chawla@hotmail.com>
2021-12-22 00:41:41 +0000

    fix

--- src/main/java/uk/gov/hmcts/ethos/replacement/docmosis/controllers/CaseActionsForCaseWorkerController.java
+++ src/main/java/uk/gov/hmcts/ethos/replacement/docmosis/controllers/CaseActionsForCaseWorkerController.java
@@ -715,8 +715,7 @@ public class CaseActionsForCaseWorkerController {
         var caseDetails = ccdRequest.getCaseDetails();
         List<String> errors = HearingsHelper.hearingMidEventValidation(caseDetails.getCaseData());
         errors.addAll(eventValidationService.validateHearingDatesNotInFuture(caseDetails.getCaseData()));
-
-
+        
         return getCallbackRespEntity(errors, caseDetails);
     }
 
