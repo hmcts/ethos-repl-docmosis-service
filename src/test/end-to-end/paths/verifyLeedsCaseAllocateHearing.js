5000455a62b3831b9be8a99acd4c548d210b2a11
hsjhita <harpreet.jhita@justice.gov.uk>
2022-01-19 17:09:19 +0000

    Re-enable Hearing tests
    Re-enable Hearing tests

--- src/test/end-to-end/paths/verifyLeedsCaseAllocateHearing.js
+++ src/test/end-to-end/paths/verifyLeedsCaseAllocateHearing.js
@@ -1,18 +1,18 @@
-// const testConfig = require('./../../config');
-// const {createCaseInCcd} = require("../helpers/ccdDataStoreApi");
-// const {eventNames} = require('../pages/common/constants.js');
-// const {acceptCaseEvent, listHearing, allocateHearing} = require("../helpers/caseHelper");
-// let caseNumber;
-//
-// Feature('Create a Leeds Single Case & Execute Allocate Hearing');
-//
-// Scenario('Verify Leeds case Allocate Hearing', async ({I}) => {
-//
-//     caseNumber = await createCaseInCcd('src/test/end-to-end/data/ccd-case-basic-data.json');
-//     await acceptCaseEvent(I, caseNumber, eventNames.ACCEPT_CASE);
-//     await listHearing(I, eventNames.LIST_HEARING, 'Leeds');
-//     await allocateHearing(I, eventNames.ALLOCATE_HEARING, 'Leeds');
-//
-// }).tag('@e2e')
-//     .tag('@leeds')
-//     .retry(testConfig.TestRetryScenarios);
+const testConfig = require('./../../config');
+const {createCaseInCcd} = require("../helpers/ccdDataStoreApi");
+const {eventNames} = require('../pages/common/constants.js');
+const {acceptCaseEvent, listHearing, allocateHearing} = require("../helpers/caseHelper");
+let caseNumber;
+
+Feature('Create a Leeds Single Case & Execute Allocate Hearing');
+
+Scenario('Verify Leeds case Allocate Hearing', async ({I}) => {
+
+    caseNumber = await createCaseInCcd('src/test/end-to-end/data/ccd-case-basic-data.json');
+    await acceptCaseEvent(I, caseNumber, eventNames.ACCEPT_CASE);
+    await listHearing(I, eventNames.LIST_HEARING, 'Leeds');
+    await allocateHearing(I, eventNames.ALLOCATE_HEARING, 'Leeds');
+
+}).tag('@e2e')
+    .tag('@leeds')
+    .retry(testConfig.TestRetryScenarios);
--- src/test/end-to-end/paths/verifyLeedsCaseHearingDetails.js
+++ src/test/end-to-end/paths/verifyLeedsCaseHearingDetails.js
@@ -1,19 +1,19 @@
-// const testConfig = require('./../../config');
-// const {createCaseInCcd} = require("../helpers/ccdDataStoreApi");
-// const {eventNames} = require('../pages/common/constants.js');
-// const {acceptCaseEvent, listHearing, allocateHearing, hearingDetails} = require("../helpers/caseHelper");
-// let caseNumber;
-//
-// Feature('Create a Leeds Single Case & Execute Hearing details');
-//
-// Scenario('Verify Leeds case Hearing details', async ({I}) => {
-//
-//     caseNumber = await createCaseInCcd('src/test/end-to-end/data/ccd-case-basic-data.json');
-//     await acceptCaseEvent(I, caseNumber, eventNames.ACCEPT_CASE);
-//     await listHearing(I, eventNames.LIST_HEARING, 'Leeds');
-//     await allocateHearing(I, eventNames.ALLOCATE_HEARING, 'Leeds');
-//     await hearingDetails(I, eventNames.HEARING_DETAILS);
-//
-// }).tag('@e2e')
-//     .tag('@leeds')
-//     .retry(testConfig.TestRetryScenarios);
+const testConfig = require('./../../config');
+const {createCaseInCcd} = require("../helpers/ccdDataStoreApi");
+const {eventNames} = require('../pages/common/constants.js');
+const {acceptCaseEvent, listHearing, allocateHearing, hearingDetails} = require("../helpers/caseHelper");
+let caseNumber;
+
+Feature('Create a Leeds Single Case & Execute Hearing details');
+
+Scenario('Verify Leeds case Hearing details', async ({I}) => {
+
+    caseNumber = await createCaseInCcd('src/test/end-to-end/data/ccd-case-basic-data.json');
+    await acceptCaseEvent(I, caseNumber, eventNames.ACCEPT_CASE);
+    await listHearing(I, eventNames.LIST_HEARING, 'Leeds');
+    await allocateHearing(I, eventNames.ALLOCATE_HEARING, 'Leeds');
+    await hearingDetails(I, eventNames.HEARING_DETAILS);
+
+}).tag('@e2e')
+    .tag('@leeds')
+    .retry(testConfig.TestRetryScenarios);
