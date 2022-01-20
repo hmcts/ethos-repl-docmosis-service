c9c7685e943cd2c7f7e6424735167c8a4f8c8edc
hsjhita <harpreet.jhita@justice.gov.uk>
2022-01-20 14:11:44 +0000

    Test and refactor

--- src/test/java/uk/gov/hmcts/ethos/replacement/docmosis/service/JudgmentValidationServiceTest.java
+++ src/test/java/uk/gov/hmcts/ethos/replacement/docmosis/service/JudgmentValidationServiceTest.java
@@ -4,13 +4,17 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import uk.gov.hmcts.ecm.common.model.bulk.types.DynamicValueType;
 import uk.gov.hmcts.ecm.common.model.ccd.CaseDetails;
+import uk.gov.hmcts.ethos.replacement.docmosis.helpers.DynamicListHelper;
 import uk.gov.hmcts.ethos.replacement.docmosis.helpers.dynamiclists.DynamicJudgements;
 
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.text.ParseException;
 import java.util.Objects;
+
+import static uk.gov.hmcts.ethos.replacement.docmosis.helpers.dynamiclists.DynamicJudgements.NO_HEARINGS;
 
 class JudgmentValidationServiceTest {
 
@@ -37,6 +41,18 @@ class JudgmentValidationServiceTest {
         caseDetails1.getCaseData().getJudgementCollection().get(0).getValue().getDynamicJudgementHearing().setValue(caseDetails1.getCaseData().getJudgementCollection().get(0).getValue().getDynamicJudgementHearing().getListItems().get(0));
         judgmentValidationService.validateJudgments(caseDetails1.getCaseData());
         Assertions.assertEquals("2019-11-01", caseDetails1.getCaseData().getJudgementCollection().get(0).getValue().getJudgmentHearingDate());
+    }
 
+    @Test
+    void populateJudgmentZeroHearings() throws ParseException {
+        var caseData = caseDetails1.getCaseData();
+        caseData.setHearingCollection(null);
+        DynamicJudgements.dynamicJudgements(caseData);
+        var dynamicValue = DynamicListHelper.getDynamicValue(NO_HEARINGS);
+        Assertions.assertEquals(dynamicValue, caseData.getJudgementCollection().get(0).getValue().getDynamicJudgementHearing().getListItems().get(0));
+
+        judgmentValidationService.validateJudgments(caseDetails1.getCaseData());
+        Assertions.assertNull(caseData.getJudgementCollection().get(0).getValue().getDynamicJudgementHearing());
+        Assertions.assertNull(caseData.getJudgementCollection().get(0).getValue().getJudgmentHearingDate());
     }
 }
