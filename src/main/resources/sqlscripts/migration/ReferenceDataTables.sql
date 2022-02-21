8e120c59b4d669d92adcb5d90e1c908d6f5ff2a9
gauravchawla <gaurav-chawla@hotmail.com>
2022-02-21 12:50:02 +0000

    add file

---
+++ src/main/resources/sqlscripts/migration/ReferenceDataTables.sql
@@ -0,0 +1,9 @@
+CREATE TYPE emp_status AS ENUM('SALARIED', 'FEE_PAID');
+
+CREATE TABLE judge (
+                       id SERIAL PRIMARY KEY,
+                       tribunal_office VARCHAR(100),
+                       code VARCHAR(100),
+                       name VARCHAR(100),
+                       employment_status emp_status
+);
