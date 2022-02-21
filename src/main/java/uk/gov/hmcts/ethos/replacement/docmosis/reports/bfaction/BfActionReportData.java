2a403fc170f22327feabcfcc42a60ee9476e6a2f
btensay <tensay.bulcha@gmail.com>
2022-02-21 13:54:22 +0000

    ecm-137 additional improvements

--- src/main/java/uk/gov/hmcts/ethos/replacement/docmosis/reports/bfaction/BfActionReportData.java
+++ src/main/java/uk/gov/hmcts/ethos/replacement/docmosis/reports/bfaction/BfActionReportData.java
@@ -1,16 +1,12 @@
 package uk.gov.hmcts.ethos.replacement.docmosis.reports.bfaction;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Getter;
 import lombok.Setter;
-import lombok.extern.slf4j.Slf4j;
 import uk.gov.hmcts.ecm.common.model.listing.ListingData;
 
-@Slf4j
-@Getter
 @Setter
-@JsonIgnoreProperties(ignoreUnknown = true)
+@Getter
 public class BfActionReportData extends ListingData {
     @JsonIgnore
     private String office;
