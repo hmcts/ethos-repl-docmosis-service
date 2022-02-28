6b82b14ec2f570ab5d24c7debe9af36341b47bc8
hsjhita <harpreet.jhita@justice.gov.uk>
2022-02-28 14:00:20 +0000

    ECM-620

--- src/test/java/uk/gov/hmcts/ethos/replacement/docmosis/service/excel/MultipleTransferServiceTest.java
+++ src/test/java/uk/gov/hmcts/ethos/replacement/docmosis/service/excel/MultipleTransferServiceTest.java
@@ -1,33 +1,50 @@
 package uk.gov.hmcts.ethos.replacement.docmosis.service.excel;
 
+import lombok.RequiredArgsConstructor;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import lombok.RequiredArgsConstructor;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import uk.gov.hmcts.ecm.common.model.ccd.CaseData;
+import uk.gov.hmcts.ecm.common.model.ccd.SubmitEvent;
+import uk.gov.hmcts.ecm.common.model.ccd.items.BFActionTypeItem;
+import uk.gov.hmcts.ecm.common.model.ccd.items.DateListedTypeItem;
+import uk.gov.hmcts.ecm.common.model.ccd.items.HearingTypeItem;
+import uk.gov.hmcts.ecm.common.model.ccd.types.BFActionType;
+import uk.gov.hmcts.ecm.common.model.ccd.types.DateListedType;
+import uk.gov.hmcts.ecm.common.model.ccd.types.HearingType;
 import uk.gov.hmcts.ecm.common.model.multiples.MultipleDetails;
 import uk.gov.hmcts.ecm.common.model.multiples.SubmitMultipleEvent;
 import uk.gov.hmcts.ecm.common.model.multiples.items.CaseMultipleTypeItem;
 import uk.gov.hmcts.ethos.replacement.docmosis.helpers.MultipleUtil;
-import uk.gov.hmcts.ethos.replacement.docmosis.service.PersistentQHelperService;
 import uk.gov.hmcts.ethos.replacement.docmosis.helpers.MultiplesHelper;
+import uk.gov.hmcts.ethos.replacement.docmosis.service.CaseTransferService;
+import uk.gov.hmcts.ethos.replacement.docmosis.service.PersistentQHelperService;
+
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.TreeMap;
 
 import static org.junit.Assert.assertEquals;
 import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.isA;
 import static org.mockito.Mockito.any;
 import static org.mockito.Mockito.anyList;
+import static org.mockito.Mockito.doCallRealMethod;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoMoreInteractions;
 import static org.mockito.Mockito.when;
+import static uk.gov.hmcts.ecm.common.model.helper.Constants.ACCEPTED_STATE;
+import static uk.gov.hmcts.ecm.common.model.helper.Constants.HEARING_STATUS_LISTED;
 import static uk.gov.hmcts.ecm.common.model.helper.Constants.MIGRATION_CASE_SOURCE;
 import static uk.gov.hmcts.ecm.common.model.helper.Constants.UPDATING_STATE;
 import static uk.gov.hmcts.ecm.common.model.helper.Constants.YES;
+import static uk.gov.hmcts.ethos.replacement.docmosis.service.CaseTransferService.BF_ACTIONS_ERROR_MSG;
+import static uk.gov.hmcts.ethos.replacement.docmosis.service.CaseTransferService.HEARINGS_ERROR_MSG;
 
 @RequiredArgsConstructor
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -41,6 +58,10 @@ public class MultipleTransferServiceTest {
     private PersistentQHelperService persistentQHelperService;
     @Mock
     private MultipleCasesReadingService multipleCasesReadingService;
+    @Mock
+    private SingleCasesReadingService singleCasesReadingService;
+    @Mock
+    private CaseTransferService caseTransferService;
 
     @InjectMocks
     private MultipleTransferService multipleTransferService;
@@ -131,6 +152,48 @@ public class MultipleTransferServiceTest {
                 + "flag2=null, flag3=null, flag4=null)", caseMultipleTypeItemList.get(1).getValue().toString());
         assertEquals("MultipleObjectType(ethosCaseRef=245004/2020, subMultiple=245002, flag1=null, "
                 + "flag2=null, flag3=null, flag4=null)", caseMultipleTypeItemList.get(2).getValue().toString());
+
+    }
+
+    @Test
+    public void validateCasesBeforeTransfer() {
+        List<String> errors = new ArrayList<>();
+        var caseData = new CaseData();
+        caseData.setEthosCaseReference("245004/2020");
+
+        // Un cleared BF Action
+        var bfActionType = new BFActionType();
+        bfActionType.setDateEntered("2020-11-11");
+        var bfActionTypeItem = new BFActionTypeItem();
+        bfActionTypeItem.setValue(bfActionType);
+        caseData.setBfActions(List.of(bfActionTypeItem));
+
+        // Listed hearing
+        var dateListedType = new DateListedType();
+        dateListedType.setHearingStatus(HEARING_STATUS_LISTED);
+        var dateListedTypeItem = new DateListedTypeItem();
+        dateListedTypeItem.setValue(dateListedType);
+        var hearingType = new HearingType();
+        hearingType.setHearingDateCollection(List.of(dateListedTypeItem));
+        var hearingTypeItem = new HearingTypeItem();
+        hearingTypeItem.setValue(hearingType);
+        caseData.setHearingCollection(List.of(hearingTypeItem));
+
+        var submitEvent = new SubmitEvent();
+        submitEvent.setCaseData(caseData);
+        submitEvent.setState(ACCEPTED_STATE);
+        submitEvent.setCaseId(1232121232);
+
+        when(excelReadingService.readExcel(anyString(), anyString(), anyList(), any(), any()))
+                .thenReturn(multipleObjects);
+        when(singleCasesReadingService.retrieveSingleCases(anyString(), anyString(), anyList(), anyString()))
+                .thenReturn(new ArrayList<>(Collections.singletonList(submitEvent)));
+        doCallRealMethod().when(caseTransferService).validateCase(isA(CaseData.class), anyList());
+
+        caseTransferService.validateCase(caseData, errors);
+        assertEquals(2, errors.size());
+        assertEquals(String.format(BF_ACTIONS_ERROR_MSG, caseData.getEthosCaseReference()), errors.get(0));
+        assertEquals(String.format(HEARINGS_ERROR_MSG, caseData.getEthosCaseReference()), errors.get(1));
 
     }
 
