77f699d57edad6b3e5a439801ffa2a92b972a845
Cindy Chan <94921454+cindychmcts@users.noreply.github.com>
2022-03-23 17:09:38 +0000

    Update CaseCloseServiceTest.java

--- src/test/java/uk/gov/hmcts/ethos/replacement/docmosis/service/CaseCloseServiceTest.java
+++ src/test/java/uk/gov/hmcts/ethos/replacement/docmosis/service/CaseCloseServiceTest.java
@@ -1,13 +1,21 @@
 package uk.gov.hmcts.ethos.replacement.docmosis.service;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import uk.gov.hmcts.ecm.common.model.ccd.CaseData;
+import uk.gov.hmcts.ecm.common.model.ccd.CaseDetails;
+import uk.gov.hmcts.ethos.replacement.docmosis.helpers.BFHelperTest;
 
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static uk.gov.hmcts.ecm.common.model.helper.Constants.CASE_CLOSED_POSITION;
+import static uk.gov.hmcts.ethos.replacement.docmosis.service.CaseCloseService.CLOSING_CASE_WITH_BF_OPEN_ERROR;
 import static uk.gov.hmcts.ethos.replacement.docmosis.service.CaseCloseService.REINSTATE_CANNOT_CASE_CLOSED_ERROR_MESSAGE;
 
 class CaseCloseServiceTest {
@@ -18,7 +26,8 @@ class CaseCloseServiceTest {
     @BeforeEach
     public void setup() throws Exception {
         caseCloseService = new CaseCloseService();
-        caseData = new CaseData();
+        CaseDetails caseDetails = generateCaseDetails("caseDetailsTest1.json");
+        caseData = caseDetails.getCaseData();
     }
 
     @Test
@@ -35,4 +44,39 @@ class CaseCloseServiceTest {
         List<String> errors = caseCloseService.validateReinstateClosedCaseMidEvent(caseData);
         assertEquals(0, errors.size());
     }
+
+    @Test
+    void shouldValidateBfActionsForCaseCloseEvent_AllCleared_Pass() {
+        List<String> errors = new ArrayList<>();
+        caseData.setBfActions(BFHelperTest.generateBFActionTypeItems());
+        caseData.getBfActions().get(0).getValue().setCleared("2022-02-22");
+        caseCloseService.validateBfActionsForCaseCloseEvent(caseData, errors);
+        assertEquals(0, errors.size());
+    }
+
+    @Test
+    void shouldValidateBfActionsForCaseCloseEvent_NotCleared_Error() {
+        List<String> errors = new ArrayList<>();
+        caseData.setBfActions(BFHelperTest.generateBFActionTypeItems());
+        caseData.getBfActions().get(0).getValue().setCleared(null);
+        caseCloseService.validateBfActionsForCaseCloseEvent(caseData, errors);
+        assertEquals(1, errors.size());
+        assertEquals(CLOSING_CASE_WITH_BF_OPEN_ERROR, errors.get(0));
+    }
+
+    @Test
+    void shouldValidateBfActionsForCaseCloseEvent_NoBF_Pass() {
+        List<String> errors = new ArrayList<>();
+        caseData.setBfActions(null);
+        caseCloseService.validateBfActionsForCaseCloseEvent(caseData, errors);
+        assertEquals(0, errors.size());
+    }
+
+    private CaseDetails generateCaseDetails(String jsonFileName) throws Exception {
+        String json = new String(Files.readAllBytes(Paths.get(Objects.requireNonNull(getClass().getClassLoader()
+                .getResource(jsonFileName)).toURI())));
+        ObjectMapper mapper = new ObjectMapper();
+        return mapper.readValue(json, CaseDetails.class);
+    }
+
 }
