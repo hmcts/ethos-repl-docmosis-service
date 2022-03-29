a39e18ccf6a47ad499be2bf3c824d0214cf85d53
gauravchawla <gaurav-chawla@hotmail.com>
2022-03-30 00:01:20 +0100

    issue fixed

--- src/test/java/uk/gov/hmcts/ethos/replacement/docmosis/helpers/HearingsHelperTest.java
+++ src/test/java/uk/gov/hmcts/ethos/replacement/docmosis/helpers/HearingsHelperTest.java
@@ -2,7 +2,7 @@ package uk.gov.hmcts.ethos.replacement.docmosis.helpers;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.time.LocalDateTime;
-import java.time.ZoneId;
+import static java.time.ZoneOffset.UTC;
 import org.junit.Before;
 import org.junit.Test;
 import uk.gov.hmcts.ecm.common.model.ccd.CaseDetails;
@@ -180,22 +180,12 @@ public class HearingsHelperTest {
         assertEquals(0, errors.size());
     }
 
-
     @Test
-    public void validateHearingDatesConsideringDSTTest() {
-        ZoneId zoneId = ZoneId.of("Europe/London");
-        caseDetails1.getCaseData().getHearingCollection().get(0).getValue().getHearingDateCollection()
-                .get(0).getValue().setHearingTimingBreak(LocalDateTime.now().atZone(zoneId).toLocalDateTime().minusMinutes(25).toString());
-        caseDetails1.getCaseData().getHearingCollection().get(0).getValue().getHearingDateCollection()
-                .get(0).getValue().setHearingTimingResume(LocalDateTime.now().atZone(zoneId).toLocalDateTime().minusMinutes(25).toString());
-        caseDetails1.getCaseData().getHearingCollection().get(0).getValue().getHearingDateCollection()
-                .get(0).getValue().setHearingTimingFinish(LocalDateTime.now().atZone(zoneId).toLocalDateTime().minusMinutes(20).toString());
-        caseDetails1.getCaseData().getHearingCollection().get(0).getValue().getHearingDateCollection()
-                .get(0).getValue().setHearingTimingStart(LocalDateTime.now().atZone(zoneId).toLocalDateTime().minusMinutes(30).toString());
-        caseDetails1.getCaseData().getHearingCollection().get(0).getValue()
-                .getHearingDateCollection().get(0).getValue().setHearingStatus(HEARING_STATUS_HEARD);
-        List<String> errors = HearingsHelper.hearingTimeValidation(caseDetails1.getCaseData());
-        assertEquals(0, errors.size());
+    public void validateIsDateInFutureConsideringDST() {
+        LocalDateTime dateTime = LocalDateTime.now().minusMinutes(25);
+        LocalDateTime now = LocalDateTime.now(UTC);
+        boolean val = HearingsHelper.isDateInFuture(dateTime.toString(), now);
+        assertFalse(val);
     }
 
     @Test
