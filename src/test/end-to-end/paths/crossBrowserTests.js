9f1409811e85333aa4b2cf489327b4429c6c453f
PhaniP <phani.perla@hmcts.net>
2022-11-24 15:49:53 +0000

    code optimization

--- src/test/end-to-end/paths/crossBrowserTests.js
+++ src/test/end-to-end/paths/crossBrowserTests.js
@@ -2,23 +2,20 @@ const testConfig = require('./../../config');
 const {createCaseInCcd} = require("../helpers/ccdDataStoreApi");
 const {navigateCase} = require("../helpers/caseHelper");
 const {eventNames} = require('../pages/common/constants.js');
-const {acceptCaseTest, caseDetails} = require("../helpers/caseHelper");
+const {acceptCaseEvent, caseDetails} = require("../helpers/caseHelper");
 let caseId;
 
 Feature('Leeds Singles Case and move to Accepted state');
 BeforeSuite(async ({I}) => caseId = await createCaseInCcd('test/end-to-end/data/ccd-case-basic-data.json'));
 
-Before(async ({I}) => {
-    await navigateCase(I, caseId);
-});
-
 Scenario('Verify Accept Case', async ({I}) => {
-    await acceptCaseTest(I, caseId, eventNames.ACCEPT_CASE);
+    await acceptCaseEvent(I, caseId, eventNames.ACCEPT_CASE);
 
 }).tag('@xb')
     .retry(testConfig.TestRetryScenarios);
 
 Scenario('Leeds Singles Case and move to Case Details state', async ({I}) => {
+    await navigateCase(I, caseId);
     await caseDetails(I, caseId, eventNames.CASE_DETAILS, 'A Clerk', 'Casework Table', 'Standard Track');
 
 }).tag('@xb')
