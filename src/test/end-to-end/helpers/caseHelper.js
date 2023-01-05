4b8fcaff135ef1edc0239ab74e3c9a70c88aad38
PhaniP <phani.perla@hmcts.net>
2023-01-05 12:06:42 +0000

    EM-1075-Dependency Check Fix

--- src/test/end-to-end/helpers/caseHelper.js
+++ src/test/end-to-end/helpers/caseHelper.js
@@ -182,6 +182,7 @@ async function acceptCaseTest(I, caseId, eventName) {
 
 async function navigateCase(I, caseId) {
     await I.authenticateWithIdam();
+    console.log("Environment ==>::" + await I.grabCurrentUrl());
     await I.amOnPage('/case-details/' + caseId);
     await I.wait(testConfig.TestTimeToWait);
 }
