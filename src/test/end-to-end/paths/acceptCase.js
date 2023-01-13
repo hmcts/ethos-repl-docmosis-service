f6102fcdd60835706d42a5e558c250f9f3f3015f
PhaniP <phani.perla@hmcts.net>
2023-01-13 13:30:06 +0000

    rearranged test execution order

--- src/test/end-to-end/paths/acceptCase.js
+++ src/test/end-to-end/paths/acceptCase.js
@@ -3,13 +3,13 @@ const {navigateCase} = require("../helpers/caseHelper");
 const {eventNames} = require('../pages/common/constants.js');
 const {acceptCaseTest} = require("../helpers/caseHelper");
 
-Feature('Leeds Singles Case and move to Accepted state');
+Feature('Accept Case');
 
 Before(async ({I}) => {
     await navigateCase(I, testConfig.CCDCaseId);
 });
 
-Scenario('Verify Accept Case', async ({I}) => {
+Scenario('Leeds Singles Case and move to Accepted state', async ({I}) => {
     await acceptCaseTest(I, testConfig.CCDCaseId, eventNames.ACCEPT_CASE);
 
 }).tag('@e2e')
