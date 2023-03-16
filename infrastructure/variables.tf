a04e03cef34fea3909153505fbd3265db810ff26
gauravchawla <gaurav-chawla@hotmail.com>
2023-03-16 14:06:47 +0000

    fix

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -1,93 +1,89 @@
-variable product {
-  default = "ethos"
+variable "product" {
+  default = ethos
 }
 
-variable location {
-  default = "UK South"
+variable "location" {
+  default = UKSouth
 }
 
-variable env {
+variable "env" {
 }
 
-variable tornado_url {
+variable "tornado_url" {
   default = "http://tornado:8090/rs/render"
 }
 
-variable subscription {
+variable "subscription" {
 }
 
-variable ilbIp {
-  default = ""
-}
+variable "ilbIp" {}
 
-variable tenant_id {
+variable "tenant_id" {}
 
-}
-
-variable jenkins_AAD_objectId {
+variable "jenkins_AAD_objectId" {
   description = "(Required) The Azure AD object ID of a user, service principal or security group in the Azure Active Directory tenant for the vault. The object ID must be unique for the list of access policies."
 }
 
-variable capacity {
+variable "capacity" {
   default = "1"
 }
 
-variable deployment_namespace {
+variable "deployment_namespace" {
   default = ""
 }
 
-variable common_tags {
+variable "common_tags" {
   type = map(string)
 }
 
-variable autoheal {
+variable "autoheal" {
   description = "Enabling Proactive Auto Heal for Webapps"
   default     = "True"
 }
 
-variable idam_api_url {
+variable "idam_api_url" {
   default = "http://sidam-api:5000"
 }
 
-variable ccd_data_store_api_url {
+variable "ccd_data_store_api_url" {
   default = "http://ccd-data-store-api:4452"
 }
 
-variable dm_url {
+variable "dm_url" {
   default = "http://dm-store:8080"
 }
 
-variable s2s_url {
+variable "s2s_url" {
   default = "http://service-auth-provider-api:8080"
 }
 
-variable micro_service {
+variable "micro_service" {
   default = "ethos_repl_service"
 }
 
-variable ccd_gateway_url {
+variable "ccd_gateway_url" {
   default = "http://127.0.0.1:3453"
 }
 
-variable component {
+variable "component" {
   default = "repl-docmosis-backend"
 }
 
-variable location_api {
+variable "location_api" {
   default = "UK South"
 }
 
-variable appinsights_location {
-  default     = "West Europe"
-  description = "Location for Application Insights"
+variable "appinsights_location" {
+  default     = 'West Europe'
+  description = 'Location for Application Insights'
 }
 
-variable enable_ase {
+variable "enable_ase" {
   default = false
 }
 
-variable managed_identity_rg_name {
+variable "managed_identity_rg_name" {
 }
 
-variable managed_identity_name {
+variable "managed_identity_name" {
 }
