5c2ed7dd2603c86365d5c2e644fa1465cab7013f
Harpreet Jhita <harpreet.jhita@justice.gov.uk>
2023-03-30 11:44:04 +0100

    Change source

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -34,7 +34,7 @@ data "azurerm_subnet" "postgres" {
 }
 
 module "repl-docmosis-backend" {
-  source       = "git@github.com:hmcts/cnp-module-webapp?ref=master"
+  source       = "git@github.com:hmcts/cnp-module-webapp?ref=Ethos-update"
   product      = "${var.product}-${local.app}"
   location     = var.location
   env          = var.env
