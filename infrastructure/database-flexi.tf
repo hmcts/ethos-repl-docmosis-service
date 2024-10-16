5fa60ae4f87fd0cabf0315f36477b1b9d797f99d
ghs_SchYQr1qJVVfuWZNwQym5aG8bUKzTe1Tls6z <62423932+hmcts-jenkins-d-to-i[bot]@users.noreply.github.com>
2024-10-16 15:36:03 +0000

    Updating Terraform Formatting

--- infrastructure/database-flexi.tf
+++ infrastructure/database-flexi.tf
@@ -15,8 +15,8 @@ module "postgres" {
       name : "ethos"
     }
   ]
-  pgsql_version        = "15"
-  admin_user_object_id = var.jenkins_AAD_objectId
+  pgsql_version                  = "15"
+  admin_user_object_id           = var.jenkins_AAD_objectId
   force_user_permissions_trigger = "1"
 }
 
