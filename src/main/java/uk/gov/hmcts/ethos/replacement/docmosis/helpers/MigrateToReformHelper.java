fcae84f45643b00ab837883e4fe9c5f7d8865c1c
Harpreet Jhita <harpreet.jhita@justice.gov.uk>
2025-03-31 18:02:31 +0100

    Update MigrateToReformHelper.java

--- src/main/java/uk/gov/hmcts/ethos/replacement/docmosis/helpers/MigrateToReformHelper.java
+++ src/main/java/uk/gov/hmcts/ethos/replacement/docmosis/helpers/MigrateToReformHelper.java
@@ -396,7 +396,7 @@ public class MigrateToReformHelper {
                 .map(doc -> {
                     DocumentTypeItem documentTypeItem = (DocumentTypeItem) objectMapper(doc, DocumentTypeItem.class);
                     DocumentType value = documentTypeItem.getValue();
-                    if (isNullOrEmpty(value.getDocumentType())) {
+                    if (isNullOrEmpty(value.getDocumentType()) && isNullOrEmpty(value.getTypeOfDocument()))) {
                         value.setTopLevelDocuments(MISC);
                         value.setMiscDocuments("Needs updating");
                         value.setDocumentType("Needs updating");
