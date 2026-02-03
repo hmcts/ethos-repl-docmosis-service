bfe4f3f19465b6f5e81f48ff5206b38eff43161a
Harpreet Jhita <harpreet.jhita@justice.gov.uk>
2026-02-03 08:53:17 +0000

    Add import for Objects utility to enhance null checks

--- src/main/java/uk/gov/hmcts/ethos/replacement/docmosis/service/CaseManagementForCaseWorkerService.java
+++ src/main/java/uk/gov/hmcts/ethos/replacement/docmosis/service/CaseManagementForCaseWorkerService.java
@@ -33,6 +33,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 import java.util.UUID;
 import java.util.stream.Stream;
