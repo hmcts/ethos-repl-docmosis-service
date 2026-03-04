ca53a60c112db558aa58e76b41dab01a931adf85
Jason Paige <j@jasonpaige.co.uk>
2026-03-04 18:25:38 +0000

    Run Flyway migrations at Spring startup

--- src/main/java/uk/gov/hmcts/ethos/replacement/docmosis/data/exception/PendingMigrationScriptException.java
+++
@@ -1,8 +0,0 @@
-package uk.gov.hmcts.ethos.replacement.docmosis.data.exception;
-
-public class PendingMigrationScriptException extends RuntimeException {
-
-    public PendingMigrationScriptException(String scriptName) {
-        super("Pending migration detected: " + scriptName);
-    }
-}
