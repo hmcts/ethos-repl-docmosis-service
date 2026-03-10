e9303cef3fa3f379410b6aa584baeb8fb79507d9
Jason Paige <j@jasonpaige.co.uk>
2026-03-10 09:31:07 +0000

    Rename old IDAM_CLIENT_SECRET to ECM_IDAM_CLIENT_SECRET to allow compatability with consumer IDAM_CLIENT_SECRET env var

--- src/test/config.js
+++ src/test/config.js
@@ -20,7 +20,7 @@ module.exports = {
     TestAutoDelayEnabled: process.env.E2E_AUTO_DELAY_ENABLED === 'true',
     TestForAccessibility: process.env.TESTS_FOR_ACCESSIBILITY === 'true',
     TestForCrossBrowser: process.env.TESTS_FOR_CROSS_BROWSER === 'true',
-    TestIdamClientSecret: process.env.IDAM_CLIENT_SECRET || '',
+    TestIdamClientSecret: process.env.ECM_IDAM_CLIENT_SECRET || '',
     TestS2SAuthSecret: process.env.SERVICE_SECRET || '',
     S2SAuthSecret: process.env.MICROSERVICE_CCD_GW || '',
     CCDCaseId: '',
