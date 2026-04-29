b3ac7f47beb89d246a5de3bbedfaefc4f0716b25
Jason Paige <j@jasonpaige.co.uk>
2026-04-29 13:42:54 +0100

    Remove legacy Service Bus sender dependency

--- src/test/java/uk/gov/hmcts/ethos/replacement/docmosis/servicebus/CreateUpdatesBusSenderTest.java
+++ src/test/java/uk/gov/hmcts/ethos/replacement/docmosis/servicebus/CreateUpdatesBusSenderTest.java
@@ -5,14 +5,10 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import uk.gov.hmcts.ecm.compat.common.model.servicebus.CreateUpdatesDto;
 import uk.gov.hmcts.ecm.compat.common.model.servicebus.datamodel.CreationDataModel;
-import uk.gov.hmcts.ecm.compat.common.servicebus.ServiceBusSender;
 import uk.gov.hmcts.ethos.replacement.docmosis.domain.repository.messagequeue.CreateUpdatesQueueRepository;
-import uk.gov.hmcts.ethos.replacement.docmosis.utils.InternalException;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -21,23 +17,16 @@ import java.util.List;
 import static org.junit.Assert.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.atLeastOnce;
-import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static uk.gov.hmcts.ecm.compat.common.model.helper.Constants.SCOTLAND_BULK_CASE_TYPE_ID;
-import static uk.gov.hmcts.ethos.replacement.docmosis.utils.InternalException.ERROR_MESSAGE;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 public class CreateUpdatesBusSenderTest {
 
-    private CreateUpdatesBusSender serviceBusCreateUpdatesBusSender;
-    private CreateUpdatesBusSender dbCreateUpdatesBusSender;
-    @Mock
-    private ServiceBusSender serviceBusSender;
+    private CreateUpdatesBusSender createUpdatesBusSender;
     @Mock
     private CreateUpdatesQueueRepository createUpdatesQueueRepository;
-    @Mock
-    private ObjectProvider<ServiceBusSender> serviceBusSenderProvider;
     @Mock
     private ObjectMapper objectMapper;
 
@@ -49,11 +38,7 @@ public class CreateUpdatesBusSenderTest {
 
     @Before
     public void setUp() {
-        when(serviceBusSenderProvider.getIfAvailable()).thenReturn(serviceBusSender);
-        serviceBusCreateUpdatesBusSender = new CreateUpdatesBusSender(serviceBusSenderProvider,
-            createUpdatesQueueRepository, objectMapper, false);
-        dbCreateUpdatesBusSender = new CreateUpdatesBusSender(serviceBusSenderProvider,
-            createUpdatesQueueRepository, objectMapper, true);
+        createUpdatesBusSender = new CreateUpdatesBusSender(createUpdatesQueueRepository, objectMapper);
         ethosCaseRefCollection = Arrays.asList("4150001/2020", "4150002/2020", "4150003/2020",
             "4150004/2020", "4150005/2020");
         createUpdatesDto = getCreateUpdatesDto(ethosCaseRefCollection);
@@ -61,33 +46,25 @@ public class CreateUpdatesBusSenderTest {
     }
 
     @Test
-    public void shouldSendToServiceBusWhenQueueDisabled() {
-        serviceBusCreateUpdatesBusSender.sendUpdatesToQueue(createUpdatesDto, creationDataModel, new ArrayList<>(),
-            String.valueOf(ethosCaseRefCollection.size()));
-        verify(serviceBusSender, atLeastOnce()).sendMessage(any());
-    }
-
-    @Test
-    public void shouldHandleServiceBusException() {
-        List<String> errors = new ArrayList<>();
-        doThrow(new InternalException(ERROR_MESSAGE))
-                .when(serviceBusSender).sendMessage(any());
-        serviceBusCreateUpdatesBusSender.sendUpdatesToQueue(createUpdatesDto, creationDataModel, errors,
-            String.valueOf(ethosCaseRefCollection.size()));
-        verify(serviceBusSender, atLeastOnce()).sendMessage(any());
-        assertEquals(1, errors.size());
-        assertEquals("Failed to send the message to the queue", errors.getFirst());
-    }
-
-    @Test
-    public void shouldSaveQueueMessagesWhenQueueEnabled() throws Exception {
+    public void shouldSaveQueueMessages() throws Exception {
         when(objectMapper.writeValueAsString(any())).thenReturn("{\"msg\":1}");
 
-        dbCreateUpdatesBusSender.sendUpdatesToQueue(createUpdatesDto, creationDataModel, new ArrayList<>(),
+        createUpdatesBusSender.sendUpdatesToQueue(createUpdatesDto, creationDataModel, new ArrayList<>(),
             String.valueOf(ethosCaseRefCollection.size()));
 
         verify(createUpdatesQueueRepository, atLeastOnce()).save(any());
-        Mockito.verifyNoInteractions(serviceBusSender);
+    }
+
+    @Test
+    public void shouldHandleQueueSaveException() throws Exception {
+        List<String> errors = new ArrayList<>();
+        when(objectMapper.writeValueAsString(any())).thenThrow(new RuntimeException("boom"));
+
+        createUpdatesBusSender.sendUpdatesToQueue(createUpdatesDto, creationDataModel, errors,
+            String.valueOf(ethosCaseRefCollection.size()));
+
+        assertEquals(1, errors.size());
+        assertEquals("Failed to send the message to the queue", errors.getFirst());
     }
 
     private CreateUpdatesDto getCreateUpdatesDto(List<String> ethosCaseRefCollection) {
